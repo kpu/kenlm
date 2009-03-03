8f42bb1dfc028bbe1347d2711b4c2f26bfc2d9c9
kpu <github@kheafield.com>
2009-03-03 22:44:46 +0000

    Also test internal nulls.
    git-svn-id: file:///dev/shm/somefilter.svn@67 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- util/tokenize_piece_test.cc
+++ util/tokenize_piece_test.cc
@@ -21,9 +21,9 @@ BOOST_AUTO_TEST_CASE(simple) {
 	BOOST_CHECK(it == kEndPieceIterator);
 }
 
-BOOST_AUTO_TEST_CASE(null) {
+BOOST_AUTO_TEST_CASE(null_delimiter) {
 	AnyCharacterDelimiter delim(StringPiece("\0", 1));
-	char str[] = "\0first\0\0second\0\0\0third\0fourth\0\0\0";
+	const char str[] = "\0first\0\0second\0\0\0third\0fourth\0\0\0";
 	PieceIterator it(StringPiece(str, sizeof(str) - 1), delim);
 	BOOST_REQUIRE(it != kEndPieceIterator);
 	BOOST_CHECK_EQUAL(StringPiece("first"), *it);
@@ -36,6 +36,21 @@ BOOST_AUTO_TEST_CASE(null) {
 	++it;
 	BOOST_REQUIRE(it != kEndPieceIterator);
 	BOOST_CHECK_EQUAL(StringPiece("fourth"), *it);
+	++it;
+	BOOST_CHECK(it == kEndPieceIterator);
+}
+
+BOOST_AUTO_TEST_CASE(null_entries) {
+	AnyCharacterDelimiter delim(" ");
+	const char str[] = "\0split\0\0 \0me\0 ";
+	PieceIterator it(StringPiece(str, sizeof(str) - 1), delim);
+	BOOST_REQUIRE(it != kEndPieceIterator);
+	const char first[] = "\0split\0\0";
+	BOOST_CHECK_EQUAL(StringPiece(first, sizeof(first) - 1), *it);
+	++it;
+	BOOST_REQUIRE(it != kEndPieceIterator);
+	const char second[] = "\0me\0";
+	BOOST_CHECK_EQUAL(StringPiece(second, sizeof(second) - 1), *it);
 	++it;
 	BOOST_CHECK(it == kEndPieceIterator);
 }
