24040427f753767a1a8d81b4fd2918f4872567f7
kpu <github@kheafield.com>
2009-03-09 05:44:59 +0000

    Time the actual find operation
    git-svn-id: file:///dev/shm/somefilter.svn@77 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- util/sorted_uniform_find.cc
+++ util/sorted_uniform_find.cc
@@ -5,21 +5,21 @@
 
 namespace util {
 namespace {
-// TODO: support 32-bit with float division.
-inline const uint64_t *Pivot(const uint64_t *left, const uint64_t *right, const uint64_t off) {
-  typedef unsigned int local_uint128_t __attribute__((mode(TI)));
-  return left + static_cast<size_t>(((local_uint128_t)off * (right - left)) / (*right - *left));
-}
 
-inline const uint32_t *Pivot(const uint32_t *left, const uint32_t *right, const uint32_t off) {
-  return left + static_cast<size_t>(((uint64_t)off * (right - left)) / (*right - *left));
+// In timing tests on Core 2 Duo and Opteron, float was faster than gcc's 128-bit support.
+inline size_t Pivot(uint64_t off, uint64_t range, size_t width) {
+  size_t ret = static_cast<size_t>(static_cast<float>(off) / static_cast<float>(range) * static_cast<float>(width));
+	// Cap for floating point rounding
+	return (ret <= width) ? ret : width;
 }
-
-inline const uint16_t *Pivot(const uint16_t *left, const uint16_t *right, const uint16_t off) {
-  return left + static_cast<size_t>(((size_t)off * (right - left)) / (*right - *left));
+inline size_t Pivot(uint32_t off, uint32_t range, size_t width) {
+	return static_cast<size_t>(static_cast<uint64_t>(off) * static_cast<uint64_t>(width) / static_cast<uint64_t>(range));
 }
-inline const uint8_t *Pivot(const uint8_t *left, const uint8_t *right, const uint8_t off) {
-  return left + static_cast<size_t>(((size_t)off * (right - left)) / (*right - *left));
+inline size_t Pivot(uint16_t off, uint16_t range, size_t width) {
+	return static_cast<size_t>(static_cast<size_t>(off) * width / static_cast<size_t>(range));
+}
+inline size_t Pivot(uint8_t off, uint8_t range, size_t width) {
+	return static_cast<size_t>(static_cast<size_t>(off) * width / static_cast<size_t>(range));
 }
 
 } // namespace
@@ -32,7 +32,7 @@ template <class Value> const Value *SortedUniformFind(const Value *left, const V
     if (key <= *left) return (key == *left) ? left : 0;
     if (key >= *right) return (key == *right) ? right : 0;
     // now *left < key < *right so there is no division by 0 here and further left < right.
-    const Value *pivot = Pivot(left, right, off);
+    const Value *pivot = left + Pivot(off, *right - *left, right - left);
     // left <= pivot < right.
     if (*pivot > key) {
       right = pivot - 1;
