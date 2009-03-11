adcbbed7e0896c21d0ed986c632c89daecfbf578
kpu <github@kheafield.com>
2009-03-11 23:50:59 +0000

    Test for ngram
    git-svn-id: file:///dev/shm/somefilter.svn@96 e102df66-1e2e-11dd-9b44-c24451a4db5e

---
+++ lm/ngram_test.cc
@@ -0,0 +1,93 @@
+#include "lm/ngram.hh"
+
+#define BOOST_TEST_MODULE NGramTest
+
+#include <boost/test/unit_test.hpp>
+
+namespace lm {
+namespace ngram {
+namespace {
+
+struct Fixture {
+	Fixture() : model("test.arpa") {}
+
+	Model model;
+
+	unsigned int Lookup(const char *value) const {
+		return model.GetVocabulary().Index(StringPiece(value));
+	}
+};
+
+BOOST_FIXTURE_TEST_SUITE(s, Fixture)
+
+BOOST_AUTO_TEST_CASE(starters) {
+	Model::State state(model.BeginSentenceState());
+	Model::State out;
+	std::vector<WordIndex> empty;
+
+	BOOST_CHECK_CLOSE(-0.4846522,
+			model.IncrementalScore(
+				state,
+				empty.rbegin(),
+				empty.rend(),
+				Lookup("looking"),
+				out).Log10(), 0.00001);
+	BOOST_CHECK_EQUAL((unsigned int)2, out.NGramLength());
+			
+	// , probability plus <s> backoff
+	BOOST_CHECK_CLOSE(-1.383514 + -0.4149733,
+			model.IncrementalScore(
+				state,
+				empty.rbegin(),
+				empty.rend(),
+				Lookup(","),
+				out).Log10(), 0.00001);
+	BOOST_CHECK_EQUAL((unsigned int)1, out.NGramLength());
+
+	BOOST_CHECK_CLOSE(-1.995635 + -0.4149733,
+			model.IncrementalScore(
+				state,
+				empty.rbegin(),
+				empty.rend(),
+				Lookup("this_is_not_found"),
+				out).Log10(), 0.00001);
+	BOOST_CHECK_EQUAL((unsigned int)0, out.NGramLength());
+}
+
+#define AppendTest(word, ngram, score) \
+	index = Lookup(word); \
+	BOOST_CHECK_CLOSE((score), \
+			model.IncrementalScore( \
+				state, \
+				history.rbegin(), \
+				history.rend(), \
+				index, \
+				out).Log10(), 0.001); \
+	BOOST_CHECK_EQUAL(static_cast<unsigned int>(ngram), out.NGramLength()); \
+	history.push_back(index);
+
+BOOST_AUTO_TEST_CASE(continuation) {
+	Model::State state(model.BeginSentenceState());
+	Model::State out;
+
+	std::vector<WordIndex> history;
+
+	WordIndex index;
+
+	AppendTest("looking", 2,-0.484652);
+	AppendTest("on", 3,-0.348837);
+	AppendTest("a", 4,-0.0155266);
+	AppendTest("little", 5,-0.00306122);
+	AppendTest("the", 1,-4.04005);
+	AppendTest("biarritz", 1,-1.9889);
+	AppendTest("not_found", 0,-2.29666);
+	AppendTest("more", 1,-1.20632);
+	AppendTest(".", 2,-0.51363);
+	AppendTest("</s>", 3,-0.0191651);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} // namespace
+} // namespace ngram
+} // namespace lm
