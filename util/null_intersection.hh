b96ba65735e8e23d299207d31a9a6f229f44c72a
kpu <github@kheafield.com>
2009-10-22 21:44:22 +0000

    Algorithm to test if intersection of multiple sets is null.
    git-svn-id: file:///dev/shm/somefilter.svn@127 e102df66-1e2e-11dd-9b44-c24451a4db5e

---
+++ util/null_intersection.hh
@@ -0,0 +1,65 @@
+#ifndef UTIL_NULL_INTERSECTION
+#define UTIL_NULL_INTERSECTION
+
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+namespace util {
+
+template <class Iterator> struct BeginEnd {
+	typedef typename std::iterator_traits<Iterator>::value_type Value;
+	BeginEnd(const Iterator &in_begin, const Iterator &in_end) : begin(in_begin), end(in_end) {}
+
+	template <unsigned size> BeginEnd(const Value (&values)[size]) : begin(values), end(values + size) {}
+	BeginEnd(const std::vector<Value> &vec) : begin(vec.begin()), end(vec.end()) {}
+
+	Iterator begin;
+	Iterator end;
+};
+
+template <class Iterator> BeginEnd<Iterator> MakeBeginEnd(const Iterator &in_begin, const Iterator &in_end) {
+	return BeginEnd<Iterator>(in_begin, in_end);
+}
+
+namespace detail {
+template <class Iterator> struct BeginEndLessBySize : public std::binary_function<const BeginEnd<Iterator> &, const BeginEnd<Iterator> &, bool> {
+  bool operator()(const BeginEnd<Iterator> &left, const BeginEnd<Iterator> &right) const {
+		return (left.end - left.begin) < (right.end - right.end);
+	}
+};
+} // namespace detail
+
+/* Takes sets specified by their iterators and returns true iff they intersect.  
+ * Each set must be sorted in increasing order.
+ * sets is left in an undefined state.
+ */
+template <class Iterator, class Less> bool NullIntersection(std::vector<BeginEnd<Iterator> > &sets, const Less &less) {
+	typedef std::vector<BeginEnd<Iterator> > Sets;
+	if (sets.empty()) return true;
+	std::sort(sets.begin(), sets.end(), detail::BeginEndLessBySize<Iterator>());
+
+	if (sets.front().begin == sets.front().end) return true;
+	Iterator highest(sets.front().begin);
+	for (typename Sets::iterator i(sets.begin()); i != sets.end(); ) {
+		i->begin = std::lower_bound(i->begin, i->end, *highest, less);
+		if (i->begin == i->end) return true;
+		if (*(i->begin) > *highest) {
+			highest = i->begin;
+			// start over
+			i = sets.begin();
+		} else {
+			++i;
+		}
+	}
+	return false;
+}
+
+template <class Iterator> bool NullIntersection(std::vector<BeginEnd<Iterator> > &sets) {
+	typedef std::less<typename std::iterator_traits<Iterator>::value_type> Less;
+  return NullIntersection<Iterator, Less>(sets, Less());
+}
+
+} // namespace util
+
+#endif // UTIL_NULL_INTERSECTION
---
+++ util/null_intersection_test.cc
@@ -0,0 +1,55 @@
+#include "util/null_intersection.hh"
+
+#define BOOST_TEST_MODULE NullIntersectionTest
+#include <boost/test/unit_test.hpp>
+
+namespace util {
+namespace {
+
+BOOST_AUTO_TEST_CASE(Empty) {
+	std::vector<BeginEnd<const unsigned int*> > sets;
+	BOOST_CHECK(NullIntersection(sets));
+	
+	sets.push_back(BeginEnd<const unsigned int*>(NULL, NULL));
+	BOOST_CHECK(NullIntersection(sets));
+}
+
+BOOST_AUTO_TEST_CASE(Single) {
+	std::vector<unsigned int> nums;
+	nums.push_back(1);
+	nums.push_back(4);
+	nums.push_back(100);
+	std::vector<BeginEnd<std::vector<unsigned int>::const_iterator> > sets;
+	sets.push_back(nums);
+
+	BOOST_CHECK(!NullIntersection(sets));
+}
+
+BOOST_AUTO_TEST_CASE(MultiNone) {
+	unsigned int nums0[] = {1, 3, 4, 22};
+	unsigned int nums1[] = {2, 5, 12};
+	unsigned int nums2[] = {4, 17};
+
+	std::vector<BeginEnd<const unsigned int*> > sets;
+	sets.push_back(nums0);
+	sets.push_back(nums1);
+	sets.push_back(nums2);
+
+	BOOST_CHECK(NullIntersection(sets));
+}
+
+BOOST_AUTO_TEST_CASE(MultiOne) {
+	unsigned int nums0[] = {1, 3, 4, 17, 22};
+	unsigned int nums1[] = {2, 5, 12, 17};
+	unsigned int nums2[] = {4, 17};
+
+	std::vector<BeginEnd<const unsigned int*> > sets;
+	sets.push_back(nums0);
+	sets.push_back(nums1);
+	sets.push_back(nums2);
+
+	BOOST_CHECK(!NullIntersection(sets));
+}
+
+} // namespace
+} // namespace util
