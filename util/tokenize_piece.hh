cc864e4158b0f4cf8f6be130b5efa480470502d3
kpu <github@kheafield.com>
2009-11-16 16:55:10 +0000

    Don't mention unicode/stringpiece directly from tokenize_piece.  Fix indentation.
    git-svn-id: file:///dev/shm/somefilter.svn@160 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- util/tokenize_piece.hh
+++ util/tokenize_piece.hh
@@ -4,7 +4,6 @@
 #include "util/string_piece.hh"
 
 #include <boost/iterator/iterator_facade.hpp>
-#include <unicode/stringpiece.h>
 
 #include <bitset>
 
@@ -20,51 +19,51 @@ namespace util {
 
 // Tokenize a StringPiece using an iterator interface.  boost::tokenizer doesn't work with StringPiece.
 template <char d> class PieceIterator : public boost::iterator_facade<PieceIterator<d>, const StringPiece, boost::forward_traversal_tag> {
-	public:
-		// Default construct is end, which is also accessed by kEndPieceIterator;
-		PieceIterator() {}
+  public:
+    // Default construct is end, which is also accessed by kEndPieceIterator;
+    PieceIterator() {}
 
-		explicit PieceIterator(const StringPiece &str)
-			  : after_(str) {
-			increment();
-		}
+    explicit PieceIterator(const StringPiece &str)
+      : after_(str) {
+        increment();
+      }
 
-		bool operator!() const {
-			return after_.data() == 0;
-		}
-		operator bool() const {
-			return after_.data() != 0;
-		}
+    bool operator!() const {
+      return after_.data() == 0;
+    }
+    operator bool() const {
+      return after_.data() != 0;
+    }
 
-		static PieceIterator<d> end() {
-			return PieceIterator<d>();
-		}
+    static PieceIterator<d> end() {
+      return PieceIterator<d>();
+    }
 
-	private:
-		friend class boost::iterator_core_access;
+  private:
+    friend class boost::iterator_core_access;
 
-		void increment() {
-			const char *start = after_.data();
-			for (; (start != after_.data() + after_.size()) && (d == *start); ++start) {}
-			if (start == after_.data() + after_.size()) {
-				// End condition.
-				after_.clear();
-				return;
-			}
-			const char *finish = start;
-			for (; (finish != after_.data() + after_.size()) && (d != *finish); ++finish) {}
-			current_ = StringPiece(start, finish - start);
-			after_ = StringPiece(finish, after_.data() + after_.size() - finish);
-		}
+    void increment() {
+      const char *start = after_.data();
+      for (; (start != after_.data() + after_.size()) && (d == *start); ++start) {}
+      if (start == after_.data() + after_.size()) {
+        // End condition.
+        after_.clear();
+        return;
+      }
+      const char *finish = start;
+      for (; (finish != after_.data() + after_.size()) && (d != *finish); ++finish) {}
+      current_ = StringPiece(start, finish - start);
+      after_ = StringPiece(finish, after_.data() + after_.size() - finish);
+    }
 
-		bool equal(const PieceIterator &other) const {
-			return after_.data() == other.after_.data();
-		}
+    bool equal(const PieceIterator &other) const {
+      return after_.data() == other.after_.data();
+    }
 
-		const StringPiece &dereference() const { return current_; }
+    const StringPiece &dereference() const { return current_; }
 
-		StringPiece current_;
-		StringPiece after_;
+    StringPiece current_;
+    StringPiece after_;
 };
 
 } // namespace util
