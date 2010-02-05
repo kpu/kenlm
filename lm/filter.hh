6cd5989b915865fc6c61911835d16c86ffd50686
kpu <github@kheafield.com>
2010-02-05 21:49:23 +0000

    Fix filter compilation for Boost 1.41
    git-svn-id: file:///dev/shm/somefilter.svn@166 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/filter.hh
+++ lm/filter.hh
@@ -12,6 +12,7 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/noncopyable.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/range/iterator_range.hpp>
 #include <boost/unordered/unordered_map.hpp>
 #include <boost/unordered/unordered_set.hpp>
 
