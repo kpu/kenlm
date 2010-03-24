723b7f3a9c7095ababde4a75b7dd8810101a6475
kpu <github@kheafield.com>
2010-03-24 04:19:34 +0000

    Fix bug in filter: if there was a space before enter, everything was read as if on one line.
    git-svn-id: file:///dev/shm/somefilter.svn@194 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/multiple_vocab.cc
+++ lm/multiple_vocab.cc
@@ -5,27 +5,31 @@
 #include "lm/multiple_vocab.hh"
 
 #include <istream>
+#include <iostream>
 
+#include <ctype.h>
 #include <err.h>
 
 namespace lm {
 
+bool IsLineEnd(std::istream &in) {
+  int got;
+  do {
+    got = in.get();
+    if (!in) return true;
+    if (got == '\n') return true;
+  } while (isspace(got));
+  in.unget();
+  return false;
+}
+
 // Read space separated words in enter separated lines.  
 void ReadMultipleVocab(std::istream &in, PrepareMultipleVocab &out) {
-  while (true) {
-    bool empty = true;
-    while (in >> out.TempStr()) {
-      out.Insert();
-      empty = false;
-      int got = in.get();
-      if (!in) break;
-      if (got == '\n') break;
+  while (in >> out.TempStr()) {
+    out.Insert();
+    if (IsLineEnd(in)) {
+      out.EndSentence();
     }
-    if (!in) {
-      if (!empty) out.EndSentence();
-      break;
-    }
-    out.EndSentence();
   }
   if (!in.eof()) {
     err(2, "Reading vocabulary");
