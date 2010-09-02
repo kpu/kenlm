28db1923d0699e0a32df689f4a57e96a96aecc98
kpu <github@kheafield.com>
2010-09-02 17:02:35 +0000

    Add (currently unused) batch mode for count_io.  Skip blank word lines.
    git-svn-id: file:///dev/shm/somefilter.svn@242 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/count_io.hh
+++ lm/count_io.hh
@@ -26,6 +26,45 @@ class CountOutput : boost::noncopyable {
     std::fstream file_;
 };
 
+class CountBatch {
+  public:
+    explicit CountBatch(std::streamsize initial_read) 
+      : initial_read_(initial_read) {
+      buffer_.reserve(initial_read);
+    }
+
+    void Read(std::istream &in) {
+      buffer_.resize(initial_read_);
+      in.read(&*buffer_.begin(), initial_read_);
+      buffer_.resize(in.gcount());
+      char got;
+      while (in.get(got) && got != '\n')
+        buffer_.push_back(got);
+    }
+
+    template <class Output> void Send(Output &out) {
+      for (util::PieceIterator<'\n'> line(StringPiece(&*buffer_.begin(), buffer_.size())); line; ++line) {
+        util::PieceIterator<'\t'> tabber(*line);
+        if (!tabber) {
+          std::cerr << "Warning: empty n-gram count line being removed\n";
+          continue;
+        }
+        util::PieceIterator<' '> words(*tabber);
+        if (!words) {
+          std::cerr << "Line has a tab but no words.\n";
+          continue;
+        }
+        out.AddNGram(words, util::PieceIterator<' '>::end(), *line);
+      }
+    }
+
+  private:
+    std::streamsize initial_read_;
+
+    // This could have been a std::string but that's less happy with raw writes.  
+    std::vector<char> buffer_;
+};
+
 template <class Output> void ReadCount(std::istream &in_file, Output &out) {
   std::string line;
   while (getline(in_file, line)) {
@@ -35,6 +74,10 @@ template <class Output> void ReadCount(std::istream &in_file, Output &out) {
       continue;
     }
     util::PieceIterator<' '> words(*tabber);
+    if (!words) {
+      std::cerr << "Line has a tab but no words.\n";
+      continue;
+    }
     out.AddNGram(words, util::PieceIterator<' '>::end(), line);
   }
   if (!in_file.eof()) {
