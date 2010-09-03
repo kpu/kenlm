eaca10faca417675fa77a85a230447d2a8b27c93
kpu <github@kheafield.com>
2010-09-03 18:20:01 +0000

    Tabs to spaces
    git-svn-id: file:///dev/shm/somefilter.svn@247 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/ngram.hh
+++ lm/ngram.hh
@@ -24,43 +24,43 @@ struct VocabularyFriend;
 } // namespace detail
 
 class Vocabulary : public base::Vocabulary {
-	public:
-	  Vocabulary() {}
+  public:
+    Vocabulary() {}
 
-		WordIndex Index(const std::string &str) const {
-			return Index(StringPiece(str));
-		}
+    WordIndex Index(const std::string &str) const {
+      return Index(StringPiece(str));
+    }
 
-		WordIndex Index(const StringPiece &str) const {
-			boost::unordered_map<StringPiece, WordIndex>::const_iterator i(ids_.find(str));
-			return (__builtin_expect(i == ids_.end(), 0)) ? not_found_ : i->second;
-		}
+    WordIndex Index(const StringPiece &str) const {
+      boost::unordered_map<StringPiece, WordIndex>::const_iterator i(ids_.find(str));
+      return (__builtin_expect(i == ids_.end(), 0)) ? not_found_ : i->second;
+    }
 
     bool Known(const StringPiece &str) const {
       return ids_.find(str) != ids_.end();
     }
 
-		const char *Word(WordIndex index) const {
-			return strings_[index].c_str();
-		}
+    const char *Word(WordIndex index) const {
+      return strings_[index].c_str();
+    }
 
-	protected:
-		// friend interface for populating.
-		friend struct detail::VocabularyFriend;
+  protected:
+    // friend interface for populating.
+    friend struct detail::VocabularyFriend;
 
-		void Reserve(size_t to) {
-			strings_.reserve(to);
-			ids_.rehash(to + 1);
-		}
+    void Reserve(size_t to) {
+      strings_.reserve(to);
+      ids_.rehash(to + 1);
+    }
 
-		WordIndex InsertUnique(std::string *word);
+    WordIndex InsertUnique(std::string *word);
 
-		void FinishedLoading();
+    void FinishedLoading();
 
-	private:
-		// TODO: optimize memory use here by using one giant buffer, preferably premade by a binary file format.
-		boost::ptr_vector<std::string> strings_;
-		boost::unordered_map<StringPiece, WordIndex> ids_;
+  private:
+    // TODO: optimize memory use here by using one giant buffer, preferably premade by a binary file format.
+    boost::ptr_vector<std::string> strings_;
+    boost::unordered_map<StringPiece, WordIndex> ids_;
 };
 
 namespace detail {
@@ -69,87 +69,87 @@ struct IdentityHash : public std::unary_function<uint64_t, size_t> {
   size_t operator()(const uint64_t key) const { return key; }
 };
 struct Prob {
-	Prob(float in_prob) : prob(in_prob) {}
-	Prob() {}
-	float prob;
+  Prob(float in_prob) : prob(in_prob) {}
+  Prob() {}
+  float prob;
 };
 struct ProbBackoff : Prob {
-	ProbBackoff(float in_prob, float in_backoff) : Prob(in_prob), backoff(in_backoff) {}
-	ProbBackoff() {}
-	float backoff;
+  ProbBackoff(float in_prob, float in_backoff) : Prob(in_prob), backoff(in_backoff) {}
+  ProbBackoff() {}
+  float backoff;
 };
 
 } // namespace detail
 // Should return the same results as SRI
 class Model : boost::noncopyable {
-	private:
-		// If you need more than 5 change this and recompile.
-		// Having this limit means that State can be
-		// (kMaxOrder - 1) * sizeof(float) bytes instead of
-		// sizeof(float*) + (kMaxOrder - 1) * sizeof(float) + malloc overhead
-		static const std::size_t kMaxOrder = 6;
+  private:
+    // If you need more than 5 change this and recompile.
+    // Having this limit means that State can be
+    // (kMaxOrder - 1) * sizeof(float) bytes instead of
+    // sizeof(float*) + (kMaxOrder - 1) * sizeof(float) + malloc overhead
+    static const std::size_t kMaxOrder = 6;
 
-	public:
-		typedef ::lm::ngram::Vocabulary Vocabulary;
+  public:
+    typedef ::lm::ngram::Vocabulary Vocabulary;
 
-		class State {
-			public:
-				State() {}
+    class State {
+      public:
+        State() {}
 
-				// Faster copying using only the valid entries.
-			  State(const State &other) : ngram_length_(other.ngram_length_) {
-					CopyValid(other);
-				}
+        // Faster copying using only the valid entries.
+        State(const State &other) : ngram_length_(other.ngram_length_) {
+          CopyValid(other);
+        }
 
-				State &operator=(const State &other) {
-					ngram_length_ = other.ngram_length_;
-					CopyValid(other);
-					return *this;
-				}
+        State &operator=(const State &other) {
+          ngram_length_ = other.ngram_length_;
+          CopyValid(other);
+          return *this;
+        }
 
-				bool operator==(const State &other) const {
-					if (ngram_length_ != other.ngram_length_) return false;
-					for (const float *first = backoff_.data(), *second = other.backoff_.data();
-							first != backoff_.data() + ValidLength(); ++first, ++second) {
-						// No arithmetic was performed on these values, so an exact comparison is justified.
-						if (*first != *second) return false;
-					}
-					return true;
-				}
+        bool operator==(const State &other) const {
+          if (ngram_length_ != other.ngram_length_) return false;
+          for (const float *first = backoff_.data(), *second = other.backoff_.data();
+              first != backoff_.data() + ValidLength(); ++first, ++second) {
+            // No arithmetic was performed on these values, so an exact comparison is justified.
+            if (*first != *second) return false;
+          }
+          return true;
+        }
 
-				unsigned char NGramLength() const { return ngram_length_; }
+        unsigned char NGramLength() const { return ngram_length_; }
 
-			private:
-				friend class Model;
-				friend size_t hash_value(const State &state);
+      private:
+        friend class Model;
+        friend size_t hash_value(const State &state);
 
-				size_t ValidLength() const {
-				  return std::min<size_t>(static_cast<size_t>(ngram_length_), kMaxOrder - 1);
-				}
+        size_t ValidLength() const {
+          return std::min<size_t>(static_cast<size_t>(ngram_length_), kMaxOrder - 1);
+        }
 
-				void CopyValid(const State &other) {
+        void CopyValid(const State &other) {
           std::copy(other.backoff_.data(), other.backoff_.data() + ValidLength(), backoff_.data());
-				}
+        }
 
-				unsigned char ngram_length_;
+        unsigned char ngram_length_;
 
-				// The first min(ngram_length_, Model::order_ - 1) entries are valid backoff weights.
-				// backoff_[0] is the backoff for unigrams.
-				// The first min(ngram_length_, kMaxOrder - 1) entries must be copied and
-				// may be used for hashing or equality.   
-				boost::array<float, kMaxOrder - 1> backoff_;
-		};
+        // The first min(ngram_length_, Model::order_ - 1) entries are valid backoff weights.
+        // backoff_[0] is the backoff for unigrams.
+        // The first min(ngram_length_, kMaxOrder - 1) entries must be copied and
+        // may be used for hashing or equality.   
+        boost::array<float, kMaxOrder - 1> backoff_;
+    };
 
-		explicit Model(const char *arpa, bool status = false);
+    explicit Model(const char *arpa, bool status = false);
 
-		const Vocabulary &GetVocabulary() const { return vocab_; }
+    const Vocabulary &GetVocabulary() const { return vocab_; }
 
-		State BeginSentenceState() const {
-			State ret;
-			ret.backoff_[0] = begin_sentence_backoff_;
-			ret.ngram_length_ = 1;
-			return ret;
-		}
+    State BeginSentenceState() const {
+      State ret;
+      ret.backoff_[0] = begin_sentence_backoff_;
+      ret.ngram_length_ = 1;
+      return ret;
+    }
 
     State NullContextState() const {
       State ret;
@@ -157,78 +157,78 @@ class Model : boost::noncopyable {
       return ret;
     }
 
-		template <class ReverseHistoryIterator> float IncrementalScore(
-			  const State &in_state,
-			  const ReverseHistoryIterator &hist_begin,
-			  const ReverseHistoryIterator &hist_end,
-			  const WordIndex new_word,
-			  State &out_state) const {
-			const unsigned int words_len = std::min<unsigned int>(in_state.NGramLength() + 1, order_);
-			uint32_t words[words_len];
-			uint32_t *const words_end = words + words_len;
-			words[0] = new_word;
-			uint32_t *dest = words + 1;
-			ReverseHistoryIterator src(hist_begin);
-			for (; dest != words_end; ++dest, ++src) {
-				if (src == hist_end) {
-					for (; dest != words_end; ++dest) {
-						*dest = vocab_.BeginSentence();
-					}
-					break;
-				}
-				*dest = *src;
-			}
-			// words is in reverse order.
-			return InternalIncrementalScore(in_state, words, words_end, out_state);
-		}
+    template <class ReverseHistoryIterator> float IncrementalScore(
+        const State &in_state,
+        const ReverseHistoryIterator &hist_begin,
+        const ReverseHistoryIterator &hist_end,
+        const WordIndex new_word,
+        State &out_state) const {
+      const unsigned int words_len = std::min<unsigned int>(in_state.NGramLength() + 1, order_);
+      uint32_t words[words_len];
+      uint32_t *const words_end = words + words_len;
+      words[0] = new_word;
+      uint32_t *dest = words + 1;
+      ReverseHistoryIterator src(hist_begin);
+      for (; dest != words_end; ++dest, ++src) {
+        if (src == hist_end) {
+          for (; dest != words_end; ++dest) {
+            *dest = vocab_.BeginSentence();
+          }
+          break;
+        }
+        *dest = *src;
+      }
+      // words is in reverse order.
+      return InternalIncrementalScore(in_state, words, words_end, out_state);
+    }
 
-		size_t Order() const { return order_; }
+    size_t Order() const { return order_; }
 
-	private: 
-		float InternalIncrementalScore(
-				const State &in_state,
-				const uint32_t *const words,
-				const uint32_t *const words_end,
-				State &out_state) const;
+  private: 
+    float InternalIncrementalScore(
+        const State &in_state,
+        const uint32_t *const words,
+        const uint32_t *const words_end,
+        State &out_state) const;
 
-		size_t order_;
+    size_t order_;
 
-		Vocabulary vocab_;
+    Vocabulary vocab_;
 
-		// Cached unigram_[vocab_.BeginSentence()].backoff
-		float begin_sentence_backoff_;
+    // Cached unigram_[vocab_.BeginSentence()].backoff
+    float begin_sentence_backoff_;
 
-		typedef std::vector<detail::ProbBackoff> Unigram;
-		Unigram unigram_;
-		
-		typedef boost::unordered_map<uint64_t, detail::ProbBackoff, detail::IdentityHash> Middle;
-		std::vector<Middle> middle_vec_; 
-		
-		typedef boost::unordered_map<uint64_t, detail::Prob, detail::IdentityHash> Longest;
-		Longest longest_;
+    typedef std::vector<detail::ProbBackoff> Unigram;
+    Unigram unigram_;
+    
+    typedef boost::unordered_map<uint64_t, detail::ProbBackoff, detail::IdentityHash> Middle;
+    std::vector<Middle> middle_vec_; 
+    
+    typedef boost::unordered_map<uint64_t, detail::Prob, detail::IdentityHash> Longest;
+    Longest longest_;
 };
 
 inline size_t hash_value(const Model::State &state) {
-	size_t ret = 0;
-	boost::hash_combine(ret, state.ngram_length_);
-	for (const float *val = state.backoff_.data(); val != state.backoff_.data() + state.ValidLength(); ++val) {
-		boost::hash_combine(ret, *val);
-	}
-	return ret;
+  size_t ret = 0;
+  boost::hash_combine(ret, state.ngram_length_);
+  for (const float *val = state.backoff_.data(); val != state.backoff_.data() + state.ValidLength(); ++val) {
+    boost::hash_combine(ret, *val);
+  }
+  return ret;
 }
 
 // This just owns Model, which in turn owns Vocabulary.  Only reason this class
 // exists is to provide the same interface as the other models.
 class Owner : boost::noncopyable {
-	public:
-		explicit Owner(const char *file_name, bool status = false) : model_(file_name, status) {}
+  public:
+    explicit Owner(const char *file_name, bool status = false) : model_(file_name, status) {}
 
-		const Vocabulary &GetVocabulary() const { return model_.GetVocabulary(); }
+    const Vocabulary &GetVocabulary() const { return model_.GetVocabulary(); }
 
-		const Model &GetModel() const { return model_; }
+    const Model &GetModel() const { return model_; }
 
-	private:
-		const Model model_;
+  private:
+    const Model model_;
 };
 
 } // namespace ngram
