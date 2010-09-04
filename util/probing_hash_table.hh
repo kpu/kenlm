04039079f93cb094f3da7eee35246e264a6a4b99
kpu <github@kheafield.com>
2010-09-04 22:50:42 +0000

    Add dummy constructor, allow assign.
    git-svn-id: file:///dev/shm/somefilter.svn@253 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- util/probing_hash_table.hh
+++ util/probing_hash_table.hh
@@ -19,72 +19,75 @@ namespace util {
  */
 
 template <class ValueT, class HashT = boost::hash<ValueT>, class EqualT = std::equal_to<ValueT>, class PointerT = const ValueT *> class ReadProbingHashTable {
-	public:
-		typedef ValueT Value;
-		typedef HashT Hash;
-		typedef EqualT Equal;
+  public:
+    typedef ValueT Value;
+    typedef HashT Hash;
+    typedef EqualT Equal;
 
-		ReadProbingHashTable(
-				PointerT start,
-				size_t buckets,
-				const Value &invalid,
-				const Hash &hash_func = Hash(),
-				const Equal &equal_func = Equal())
-			: start_(start), buckets_(buckets), invalid_(invalid), hash_(hash_func), equal_(equal_func) {}
+    ReadProbingHashTable() {}
 
-		template <class Key> const Value *Find(const Key &key) const {
-			const Value *it = start_ + (hash_(key) % buckets_);
-			while (true) {
-				if (equal_(*it, invalid_)) return NULL;
-				if (equal_(*it, key)) return it;
-				++it;
-				if (it == start_ + buckets_) it = start_;
-			}
-		}
+    ReadProbingHashTable(
+        PointerT start,
+        size_t buckets,
+        const Value &invalid,
+        const Hash &hash_func = Hash(),
+        const Equal &equal_func = Equal())
+      : start_(start), end_(start + buckets), buckets_(buckets), invalid_(invalid), hash_(hash_func), equal_(equal_func) {}
 
-	protected:
-		const PointerT start_;
-		const size_t buckets_;
-		const Value invalid_;
-		const Hash hash_;
-		const Equal equal_;
+    template <class Key> const Value *Find(const Key &key) const {
+      const Value *it = start_ + (hash_(key) % buckets_);
+      while (true) {
+        if (equal_(*it, invalid_)) return NULL;
+        if (equal_(*it, key)) return it;
+        ++it;
+        if (it == end_) it = start_;
+      }
+    }
+
+  protected:
+    PointerT start_, end_;
+    size_t buckets_;
+    Value invalid_;
+    Hash hash_;
+    Equal equal_;
 };
 
 template <class ValueT, class HashT = boost::hash<ValueT>, class EqualT = std::equal_to<ValueT> > class ProbingHashTable : public ReadProbingHashTable<ValueT, HashT, EqualT, ValueT *> {
-	private:
-		typedef ReadProbingHashTable<ValueT, HashT, EqualT, ValueT *> P;
-		
-	public:
-		// Memory should be initialized buckets copies of invalid.
-		ProbingHashTable(
-				typename P::Value *start,
-				size_t buckets,
-				const typename P::Value &invalid,
-				const typename P::Hash &hash_func = typename P::Hash(),
-				const typename P::Equal &equal_func = typename P::Equal())
-			: P(start, buckets, invalid, hash_func, equal_func) {}
+  private:
+    typedef ReadProbingHashTable<ValueT, HashT, EqualT, ValueT *> P;
+    
+  public:
+    ProbingHashTable() {}
 
-		std::pair<const typename P::Value *, bool> Insert(const typename P::Value &value) {
-			typename P::Value *it = P::start_ + (P::hash_(value) % P::buckets_);
-			while (!P::equal_(*it, P::invalid_)) {
-				if (P::equal_(*it, value)) return std::pair<const typename P::Value*, bool>(it, false);
-				++it;
-				if (it == P::start_ + P::buckets_) it = P::start_;
-			}
-			*it = value;
-			return std::pair<const typename P::Value*, bool>(it, true);
+    // Memory should be initialized buckets copies of invalid.
+    ProbingHashTable(
+        typename P::Value *start,
+        size_t buckets,
+        const typename P::Value &invalid,
+        const typename P::Hash &hash_func = typename P::Hash(),
+        const typename P::Equal &equal_func = typename P::Equal())
+      : P(start, buckets, invalid, hash_func, equal_func) {}
 
-		}
-		
-		const typename P::Value *InsertAlreadyUnique(const typename P::Value &value) {
-			typename P::Value *it = P::start_ + (P::hash_(value) % P::buckets_);
-			while (!P::equal_(*it, P::invalid_)) {
-				++it;
-				if (it == P::start_ + P::buckets_) it = P::start_;
-			}
-			*it = value;
-			return it;
-		}
+    std::pair<const typename P::Value *, bool> Insert(const typename P::Value &value) {
+      typename P::Value *it = P::start_ + (P::hash_(value) % P::buckets_);
+      while (!P::equal_(*it, P::invalid_)) {
+        if (P::equal_(*it, value)) return std::pair<const typename P::Value*, bool>(it, false);
+        ++it;
+        if (it == P::end_) it = P::start_;
+      }
+      *it = value;
+      return std::pair<const typename P::Value*, bool>(it, true);
+    }
+    
+    const typename P::Value *InsertAlreadyUnique(const typename P::Value &value) {
+      typename P::Value *it = P::start_ + (P::hash_(value) % P::buckets_);
+      while (!P::equal_(*it, P::invalid_)) {
+        ++it;
+        if (it == P::end_) it = P::start_;
+      }
+      *it = value;
+      return it;
+    }
 };
 
 } // namespace util
