a4f35a104403e09e52d8dbcee03d97fea6027a52
kpu <github@kheafield.com>
2010-09-09 22:00:39 +0000

    Nothing uses this vocab class and it's confusing.
    git-svn-id: file:///dev/shm/somefilter.svn@299 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/vocab.cc
+++
@@ -1,8 +0,0 @@
-#include "lm/vocab.hh"
-
-namespace lm {
-
-const char *const GenericVocabulary::kNotFoundWord = "<unk>";
-const unsigned int GenericVocabulary::kNotFoundIndex = 0;
-
-} // namespace lm
