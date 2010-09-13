048c33ff0522080eee2e4d250d0ce7dd20874039
kpu <github@kheafield.com>
2010-09-13 17:36:22 +0000

    Add ngram_build_binary
    git-svn-id: file:///dev/shm/somefilter.svn@336 e102df66-1e2e-11dd-9b44-c24451a4db5e

---
+++ lm/ngram_build_binary.cc
@@ -0,0 +1,13 @@
+#include "lm/ngram.hh"
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " input.arpa output.mmap" << std::endl;
+    return 1;
+  }
+  lm::ngram::Config config;
+  config.write_mmap = argv[2];
+  lm::ngram::Model(argv[1], config);
+}
