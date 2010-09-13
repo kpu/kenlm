8a512b5d49c64593da718390083562c2d693a9c5
kpu <github@kheafield.com>
2010-09-13 17:13:35 +0000

    Remove dependence on arpa_io.  At some point I'll have to merge the filter to use the faster reading.
    git-svn-id: file:///dev/shm/somefilter.svn@334 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/ngram.cc
+++ lm/ngram.cc
@@ -1,6 +1,5 @@
 #include "lm/ngram.hh"
 
-#include "lm/arpa_io.hh"
 #include "lm/exception.hh"
 #include "util/file_piece.hh"
 #include "util/joint_sort.hh"
@@ -126,6 +125,43 @@ uint64_t ChainedWordHash(const WordIndex *word, const WordIndex *word_end) {
     current = CombineWordHash(current, *word);
   }
   return current;
+}
+
+bool IsEntirelyWhiteSpace(const StringPiece &line) {
+  for (size_t i = 0; i < static_cast<size_t>(line.size()); ++i) {
+    if (!isspace(line.data()[i])) return false;
+  }
+  return true;
+}
+
+void ReadARPACounts(util::FilePiece &in, std::vector<size_t> &number) {
+  number.clear();
+  StringPiece line;
+  if (!IsEntirelyWhiteSpace(line = in.ReadLine())) UTIL_THROW(FormatLoadException, "First line was \"" << line << "\" not blank");
+  if ((line = in.ReadLine()) != "\\data\\") UTIL_THROW(FormatLoadException, "second line was \"" << line << "\" not \\data\\.");
+  while (!IsEntirelyWhiteSpace(line = in.ReadLine())) {
+    if (line.size() < 6 || strncmp(line.data(), "ngram ", 6)) UTIL_THROW(FormatLoadException, "count line \"" << line << "\"doesn't begin with \"ngram \"");
+    // So strtol doesn't go off the end of line.  
+    std::string remaining(line.data() + 6, line.size() - 6);
+    char *end_ptr;
+    unsigned long int length = std::strtol(remaining.c_str(), &end_ptr, 10);
+    if ((end_ptr == remaining.c_str()) || (length - 1 != number.size())) UTIL_THROW(FormatLoadException, "ngram count lengths should be consecutive starting with 1: " << line);
+    if (*end_ptr != '=') UTIL_THROW(FormatLoadException, "Expected = immediately following the first number in the count line " << line);
+    ++end_ptr;
+    const char *start = end_ptr;
+    long int count = std::strtol(start, &end_ptr, 10);
+    if (count < 0) UTIL_THROW(FormatLoadException, "Negative n-gram count " << count);
+    if (start == end_ptr) UTIL_THROW(FormatLoadException, "Couldn't parse n-gram count from " << line);
+    number.push_back(count);
+  }
+}
+
+void ReadNGramHeader(util::FilePiece &in, unsigned int length) {
+  StringPiece line;
+  while (IsEntirelyWhiteSpace(line = in.ReadLine())) {}
+  std::stringstream expected;
+  expected << '\\' << length << "-grams:";
+  if (line != expected.str()) UTIL_THROW(FormatLoadException, "Was expecting n-gram header " << expected.str() << " but got " << line << " instead.");
 }
 
 // Special unigram reader because unigram's data structure is different and because we're inserting vocab words.
@@ -321,7 +357,7 @@ template <class Search, class VocabularyT> GenericModel<Search, VocabularyT>::Ge
     if (config.probing_multiplier <= 1.0) UTIL_THROW(FormatLoadException, "probing multiplier must be > 1.0");
 
     util::FilePiece f(file, mapped_file_.release(), config.messages);
-    ReadCounts(f, counts);
+    ReadARPACounts(f, counts);
     size_t memory_size = Size(counts, config);
     char *start;
 
