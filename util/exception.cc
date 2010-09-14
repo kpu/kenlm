d56064c844023e61864d066fe602bb1274b80f86
kpu <github@kheafield.com>
2010-09-14 14:04:51 +0000

    Hope this version compiles on OS X.
    git-svn-id: file:///dev/shm/somefilter.svn@341 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- util/exception.cc
+++ util/exception.cc
@@ -11,16 +11,24 @@ Exception::Exception(const Exception &other) throw() : stream_(other.stream_.str
 Exception &Exception::operator=(const Exception &other) throw() { stream_.str(other.stream_.str()); return *this; }
 const char *Exception::what() const throw() { return stream_.str().c_str(); }
 
+namespace {
+// The XOPEN version.
+const char *HandleStrerror(int ret, const char *buf) {
+  if (!ret) return buf;
+  return NULL;
+}
+
+// The GNU version.
+const char *HandleStrerror(const char *ret, const char *buf) {
+  return ret;
+}
+} // namespace
+
 ErrnoException::ErrnoException() throw() : errno_(errno) {
   char buf[200];
   buf[0] = 0;
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
-  const char *add = buf;
-  if (!strerror_r(errno, buf, 200)) {
-#else
-  const char *add = strerror_r(errno, buf, 200);
+  const char *add = HandleStrerror(strerror_r(errno, buf, 200), buf);
   if (add) {
-#endif
     *this << add << ' ';
   }
 }
