5deb8c5cf70b305302576fde4e7dc2a3c2b94ac2
kpu <github@kheafield.com>
2010-09-18 21:53:03 +0000

    In theory, this builds a simple trie LM.  There's nothing to query it yet.
    git-svn-id: file:///dev/shm/somefilter.svn@355 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/sort.cc
+++ lm/sort.cc
@@ -5,6 +5,8 @@
 #include "util/scoped.hh"
 
 #include <algorithm>
+#include <cstring>
+#include <cstdio>
 #include <limits>
 //#include <parallel/algorithm>
 #include <vector>
@@ -146,19 +148,29 @@ template <class Voc, class Entry> void ReadNGram(util::FilePiece &f, const unsig
 }
 
 void WriteOrThrow(FILE *to, const void *data, size_t size) {
-  if (size != fwrite(data, 1, size, to)) UTIL_THROW(util::ErrnoException, "Short write");
+  if (size != std::fwrite(data, 1, size, to)) UTIL_THROW(util::ErrnoException, "Short write");
 }
 
-template <class Entry> void DiskFlush(const Entry *begin, const Entry *end, const std::string &file_prefix, std::size_t batch) {
+void ReadOrThrow(FILE *from, void *data, size_t size) {
+  if (size != std::fread(data, 1, size, from)) UTIL_THROW(util::ErrnoException, "Short read");
+}
+
+template <class Entry> void DiskFlush(const Entry *begin, const Entry *end, const std::string &file_prefix, std::size_t batch, bool merged) {
   std::stringstream assembled;
-  assembled << file_prefix << static_cast<unsigned int>(Entry::kOrder) << '_' << batch;
+  assembled << file_prefix << static_cast<unsigned int>(Entry::kOrder) << '_';
+  if (merged) {
+    assembled << "merged";
+  } else {
+    assembled << batch;
+  }
   util::scoped_FILE out(fopen(assembled.str().c_str(), "w"));
+  if (!out.get()) UTIL_THROW(util::ErrnoException, "Couldn't open " << assembled.str().c_str() << " for writing");
   for (const Entry *group_begin = begin; group_begin != end;) {
     const Entry *group_end = group_begin;
     for (++group_end; (group_end != end) && !memcmp(group_begin->words, group_end->words, sizeof(WordIndex) * (Entry::kOrder - 1)); ++group_end) {}
     WriteOrThrow(out.get(), group_begin->words, sizeof(WordIndex) * (Entry::kOrder - 1));
-    uint64_t group_size = group_end - group_begin;
-    WriteOrThrow(out.get(), &group_size, sizeof(uint64_t));
+    WordIndex group_size = group_end - group_begin;
+    WriteOrThrow(out.get(), &group_size, sizeof(group_size));
     for (const Entry *i = group_begin; i != group_end; ++i) {
       WriteOrThrow(out.get(), &i->words[Entry::kOrder - 1], sizeof(WordIndex));
       WriteOrThrow(out.get(), &i->weights, sizeof(typename Entry::Weights));
@@ -167,8 +179,8 @@ template <class Entry> void DiskFlush(const Entry *begin, const Entry *end, cons
   }
 }
 
-template <class Entry> void RecursiveSort(util::FilePiece &f, const lm::ngram::SortedVocabulary &vocab, const std::vector<size_t> &counts, util::scoped_memory &mem, const std::string &file_prefix) {
-  RecursiveSort<FullEntry<Entry::kOrder - 1> >(f, vocab, counts, mem, file_prefix);
+template <class Entry> void ConvertToSorted(util::FilePiece &f, const lm::ngram::SortedVocabulary &vocab, const std::vector<size_t> &counts, util::scoped_memory &mem, const std::string &file_prefix) {
+  ConvertToSorted<FullEntry<Entry::kOrder - 1> >(f, vocab, counts, mem, file_prefix);
 
   ReadNGramHeader(f, Entry::kOrder);
   const size_t count = counts[Entry::kOrder - 1];
@@ -183,12 +195,12 @@ template <class Entry> void RecursiveSort(util::FilePiece &f, const lm::ngram::S
     //__gnu_parallel::sort(begin, out_end);
     std::sort(begin, out_end);
     
-    DiskFlush(begin, out_end, file_prefix, batch);
+    DiskFlush(begin, out_end, file_prefix, batch, count == batch_size);
     done += out_end - begin;
   }
 }
 
-template <> void RecursiveSort<FullEntry<1> >(util::FilePiece &f, const lm::ngram::SortedVocabulary &vocab, const std::vector<size_t> &counts, util::scoped_memory &mem, const std::string &file_prefix) {}
+template <> void ConvertToSorted<FullEntry<1> >(util::FilePiece &f, const lm::ngram::SortedVocabulary &vocab, const std::vector<size_t> &counts, util::scoped_memory &mem, const std::string &file_prefix) {}
 
 void OpenForZeroedMMAP(const char *name, std::size_t size, util::scoped_fd &fd, util::scoped_mmap &mem) {
   fd.reset(open(name, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
@@ -197,8 +209,7 @@ void OpenForZeroedMMAP(const char *name, std::size_t size, util::scoped_fd &fd, 
   mem.reset(mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd.get(), 0), size);
 }
 
-void ARPAToSortedFiles(util::FilePiece &f, std::size_t buffer, const std::string &file_prefix) {
-  std::vector<std::size_t> counts;
+void ARPAToSortedFiles(util::FilePiece &f, std::size_t buffer, const std::string &file_prefix, std::vector<size_t> &counts) {
   ReadARPACounts(f, counts);
 
   ngram::SortedVocabulary vocab;
@@ -215,13 +226,202 @@ void ARPAToSortedFiles(util::FilePiece &f, std::size_t buffer, const std::string
 
   util::scoped_memory mem;
   mem.reset(new char[buffer], buffer, util::scoped_memory::ARRAY_ALLOCATED);
-  RecursiveSort<lm::ProbEntry<5> >(f, vocab, counts, mem, file_prefix);
+  ConvertToSorted<lm::ProbEntry<5> >(f, vocab, counts, mem, file_prefix);
+}
+
+struct MiddleValue {
+  ProbBackoff weights;
+  uint64_t next;
+  uint64_t Next() const { return next; }
+};
+
+struct EndValue {
+  Prob weights;
+  uint64_t Next() const { return 0; }
+};
+
+template <class Value> class SimpleTrie {
+  private:
+    struct Entry {
+      WordIndex key;
+      WordIndex GetKey() const { return key; }
+      Value value;
+    };
+
+  public:
+    SimpleTrie() {}
+
+    void Init(void *mem, std::size_t entries) {
+      begin_ = reinterpret_cast<const Entry*>(mem);
+      end_ = reinterpret_cast<Entry*>(mem);
+    }
+
+    bool Find(uint64_t offset, std::size_t delta, WordIndex key, Value &out, uint64_t &delta_out) const {
+      const Entry *found;
+      if (!util::SortedUniformFind(begin_ + offset, begin_ + offset + delta, key, found)) return false;
+      out = found->value;
+      delta_out = (found+1)->value.Next() - found->value.Next();
+      return true;
+    }
+
+    static std::size_t Size(std::size_t entries) {
+      return entries * sizeof(Entry);
+    }
+
+    std::size_t NextOffset() const {
+      return end_ - begin_;
+    }
+
+    class Inserter {
+      public:
+        Inserter(SimpleTrie<Value> &in, std::size_t size) : begin_(in.begin_), end_(in.end_) {}
+
+        void Add(WordIndex key, const Value &value) {
+          end_->key = key;
+          end_->value = value;
+          ++end_;
+        }
+
+      private:
+        const Entry *begin_;
+        Entry *&end_;
+    };
+
+  private:
+    friend class Inserter;
+    const Entry *begin_;
+    Entry *end_;
+};
+
+class SortedFileReader {
+  public:
+    SortedFileReader() {}
+
+    void Init(const std::string &name, unsigned char order) {
+      file_.reset(fopen(name.c_str(), "r"));
+      if (!file_.get()) UTIL_THROW(util::ErrnoException, "Opening " << name << " for read");
+      header_.resize(order - 1);
+      ReadHeader();
+    }
+
+    // Preceding words.
+    const WordIndex *Header() const {
+      return &*header_.begin();
+    }
+
+    void ReadCount(WordIndex &to) {
+      ReadOrThrow(file_.get(), &to, sizeof(WordIndex));
+    }
+
+    void ReadWord(WordIndex &to) {
+      ReadOrThrow(file_.get(), &to, sizeof(WordIndex));
+    }
+
+    template <class Weights> void ReadWeights(Weights &to) {
+      ReadOrThrow(file_.get(), &to, sizeof(Weights));
+    }
+
+  private:
+    void ReadHeader() {
+      ReadOrThrow(file_.get(), &*header_.begin(), sizeof(WordIndex) * header_.size());
+    }
+
+    util::scoped_FILE file_;
+
+    std::vector<WordIndex> header_;
+};
+
+struct RecursiveInsertParams {
+  WordIndex *words;
+  SortedFileReader *files;
+  unsigned char max_order;
+  SimpleTrie<MiddleValue> *middle;
+  SimpleTrie<EndValue> *longest;
+};
+
+uint64_t RecursiveInsert(RecursiveInsertParams &params, unsigned char order) {
+  SortedFileReader &file = params.files[order - 2];
+  if (std::memcmp(params.words, file.Header(), sizeof(WordIndex) * (order - 1)))
+    return std::numeric_limits<uint64_t>::max();
+  WordIndex count;
+  file.ReadCount(count);
+  WordIndex key;
+  uint64_t ret;
+  if (order == params.max_order) {
+    ret = params.longest->NextOffset();
+    SimpleTrie<EndValue>::Inserter inserter(*params.longest, count);
+    EndValue value;
+    for (WordIndex i = 0; i < count; ++i) {
+      file.ReadWord(key);
+      file.ReadWeights(value.weights);
+      inserter.Add(key, value);
+    }
+    return ret;
+  } else {
+    ret = params.middle[order - 2].NextOffset();
+    SimpleTrie<MiddleValue>::Inserter inserter(params.middle[order - 2], count);
+    MiddleValue value;
+    for (WordIndex i = 0; i < count; ++i) {
+      file.ReadWord(params.words[order - 1]);
+      value.next = RecursiveInsert(params, order + 1);
+      file.ReadWeights(value.weights);
+      inserter.Add(params.words[order - 1], value);
+    }
+  }
+  return ret;
+}
+
+void BuildTrie(const std::string &file_prefix, const std::vector<std::size_t> &counts) {
+  std::vector<MiddleValue> unigrams(counts[0]);
+
+  std::vector<std::vector<char> > middle_mem(counts.size() - 2);
+  std::vector<SimpleTrie<MiddleValue> > middle;
+  for (size_t i = 0; i < middle.size(); ++i) {
+    middle_mem[i].resize(SimpleTrie<MiddleValue>::Size(counts[i + 1]));
+    middle[i].Init(&*middle_mem.begin(), counts[i+1]);
+  }
+  std::vector<char> longest_mem(SimpleTrie<Prob>::Size(counts.back()));
+  SimpleTrie<EndValue> longest;
+  longest.Init(&*longest_mem.begin(), counts.back());
+
+  // Load unigrams.  Leave the next pointers uninitialized.   
+  {
+    std::string name(file_prefix + "unigrams");
+    util::scoped_FILE file(fopen(name.c_str(), "r"));
+    if (!file.get()) UTIL_THROW(util::ErrnoException, "Opening " << name << " failed");
+    for (WordIndex i = 0; i < counts[0]; ++i) {
+      ReadOrThrow(file.get(), &unigrams[i].weights, sizeof(ProbBackoff));
+    }
+  }
+
+  // inputs[0] is bigrams.
+  SortedFileReader inputs[counts.size() - 1];
+  for (unsigned char i = 2; i <= counts.size(); ++i) {
+    std::stringstream assembled;
+    assembled << file_prefix << i << "_merged";
+    inputs[i-2].Init(assembled.str(), i);
+  }
+
+  // words[0] is unigrams.  
+  WordIndex words[counts.size()];
+  RecursiveInsertParams params;
+  params.words = words;
+  params.files = inputs;
+  params.max_order = static_cast<unsigned char>(counts.size());
+  params.middle = &*middle.begin();
+  params.longest = &longest;
+
+  for (words[0] = 0; words[0] < counts[0]; ++words[0]) {
+    unigrams[words[0]].next = RecursiveInsert(params, 2);
+  }
 }
 
 } // namespace lm
 
 int main() {
   util::FilePiece f("stdin", 0);
-  lm::ARPAToSortedFiles(f, 1073741824ULL, "sort/");
+  std::vector<std::size_t> counts;
+  lm::ARPAToSortedFiles(f, 1073741824ULL, "sort/", counts);
   // TODO: merge.
+
 }
