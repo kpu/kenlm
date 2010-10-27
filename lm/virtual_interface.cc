5f0cd14617b489dac34e5e02564369c661b22f29
kpu <github@kheafield.com>
2010-10-27 01:36:53 +0000

    Finish removing available_
    git-svn-id: file:///dev/shm/somefilter.svn@445 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/virtual_interface.cc
+++ lm/virtual_interface.cc
@@ -7,11 +7,10 @@ namespace base {
 
 Vocabulary::~Vocabulary() {}
 
-void Vocabulary::SetSpecial(WordIndex begin_sentence, WordIndex end_sentence, WordIndex not_found, WordIndex available) {
+void Vocabulary::SetSpecial(WordIndex begin_sentence, WordIndex end_sentence, WordIndex not_found) {
   begin_sentence_ = begin_sentence;
   end_sentence_ = end_sentence;
   not_found_ = not_found;
-  available_ = available;
   if (begin_sentence_ == not_found_) throw SpecialWordMissingException("<s>");
   if (end_sentence_ == not_found_) throw SpecialWordMissingException("</s>");
 }
