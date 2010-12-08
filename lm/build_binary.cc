c033cc98307aef66bb5c0f3172d2537e7ca36b3a
kpu <github@kheafield.com>
2010-12-08 01:48:36 +0000

    Possibly working build binary with more options.
    git-svn-id: file:///dev/shm/somefilter.svn@469 e102df66-1e2e-11dd-9b44-c24451a4db5e

--- lm/build_binary.cc
+++ lm/build_binary.cc
@@ -1,13 +1,86 @@
 #include "lm/model.hh"
+#include "util/file_piece.hh"
 
 #include <iostream>
+#include <iomanip>
+
+#include <unistd.h>
+#include <math.h>
+
+namespace {
+void Usage(const char *name) {
+  std::cerr << "Usage: " << name << "[-u unknown_probability] [-p probing_multiplier] [-t trie_temporary] [-m trie_building_megabytes] [type] input.arpa output.mmap\n"
+"If the ARPA file does not have <unk>, -u sets the probability (default 0)\n"
+"There are three data structure options:\n\n"
+"probing uses a probing hash table.  It is the fastest but uses the most memory.\n\n"
+"probing_multiplier sets the space multiplier and must be >1.0.  The default is\n"
+"1.5.  probing is the default.\n\n"
+"trie is a straightforward trie with bit-level packing.  It uses the least\n"
+"memory and is still faster than SRI or IRST.  Building the trie format uses an\n"
+"on-disk sort to save memory.\n"
+"-t is the temporary directory prefix.  Default is the output file name.\n"
+"-m is the amount of memory to use, in MB.  Default is 1024MB (1GB).\n\n"
+"sorted is like probing but uses a sorted uniform map instead of a hash table.\n"
+"It uses more memory than trie and is also slower, so there's no real reason to\n"
+"use it.\n\n"
+"Passing only an input file will print memory usage of each data structure.\n";
+  exit(1);
+}
+} // namespace
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " input.arpa output.mmap" << std::endl;
-    return 1;
-  }
+  using namespace lm::ngram;
+
   lm::ngram::Config config;
-  config.write_mmap = argv[2];
-  lm::ngram::Model(argv[1], config);
+  int opt;
+  while ((opt = getopt(argc, argv, "u:p:t:m:")) != -1) {
+    switch(opt) {
+      case 'u':
+        config.unknown_missing_prob = atof(optarg);
+        break;
+      case 'p':
+        config.probing_multiplier = atof(optarg);
+        break;
+      case 't':
+        config.temporary_directory_prefix = optarg;
+        break;
+      case 'm':
+        config.building_memory = atol(optarg) * 1048576;
+        break;
+      default:
+        Usage(argv[0]);
+    }
+  }
+  if (optind + 1 == argc) {
+    std::vector<uint64_t> counts;
+    util::FilePiece f(argv[optind]);
+    lm::ReadARPACounts(f, counts);
+    std::size_t probing_size = ProbingModel::Size(counts, config);
+    // probing is always largest.
+    long int length = lrint(ceil(log10(probing_size)));
+    std::cout << 
+      "Memory use in bytes:\n"
+      "probing " << std::setw(length) << probing_size << "\n"
+      "trie    " << std::setw(length) << TrieModel::Size(counts, config) << "\n"
+      "sorted  " << std::setw(length) << SortedModel::Size(counts, config) << "\n";
+  } else if (optind + 2 == argc) {
+    config.write_mmap = argv[optind + 1];
+    ProbingModel(argv[optind], config);
+  } else if (optind + 3 == argc) {
+    const char *model_type = argv[optind];
+    const char *from_file = argv[optind + 1];
+    config.write_mmap = argv[optind + 2];
+    if (!strcmp(model_type, "probing")) {
+      ProbingModel(from_file, config);
+    } else if (!strcmp(model_type, "sorted")) {
+      SortedModel(from_file, config);
+    } else if (!strcmp(model_type, "trie")) {
+      TrieModel(from_file, config);
+    } else {
+      Usage(argv[0]);
+    }
+  } else {
+    Usage(argv[0]);
+  }
+  return 0;
 }
