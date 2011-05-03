e2feb2984fb9a70ab4e4f0603c788baa1c157c2e
kpu <github@kheafield.com>
2011-05-03 00:06:03 +0000

    Benchmarking
    git-svn-id: file:///dev/shm/somefilter.svn@540 e102df66-1e2e-11dd-9b44-c24451a4db5e

---
+++ lm/benchmark_score.cc
@@ -0,0 +1,80 @@
+#include "lm/model.hh"
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+#include <ctype.h>
+
+#include <unistd.h>
+
+void PrintFile(const char *name) {
+  std::ifstream proc_stat(name);
+  std::string line;
+  while (getline(proc_stat, line)) {
+    std::cerr << line << '\n';
+  }
+}
+
+void PrintStatus() {
+  PrintFile("/proc/self/status");
+  PrintFile("/proc/self/stat");
+  std::cerr << "Clocks is " << sysconf(_SC_CLK_TCK) << std::endl;
+}
+
+template <class Model> void Score(const char *name) {
+  lm::ngram::Config config;
+  config.load_method = util::READ;
+  Model m(name, config);
+  std::cerr << "Loaded:" << std::endl;
+  PrintStatus();
+  std::vector<lm::WordIndex> indices(4096);
+
+  typename Model::State state[2];
+  state[0] = m.BeginSentenceState();
+  typename Model::State *in_state = &state[0], *out_state = &state[1];
+  lm::WordIndex delimit = m.GetVocabulary().BeginSentence();
+  float total = 0.0;
+  while (true) {
+    const size_t amount = fread(&*indices.begin(), sizeof(lm::WordIndex), indices.size(), stdin);
+    if (!amount) {
+      if (feof(stdin)) break;
+      UTIL_THROW(util::ErrnoException, "Reading stdin");
+    }
+    for (const lm::WordIndex *i = &*indices.begin(); i != &*indices.begin() + amount; ++i) {
+      if (*i == delimit) {
+        *in_state = m.BeginSentenceState();
+        continue;
+      }
+      total += m.FullScore(*in_state, *i, *out_state).prob;
+      std::swap(in_state, out_state);
+    }
+  }
+  std::cout << total << std::endl;
+  std::cerr << "Scored:" << std::endl;
+  PrintStatus();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) UTIL_THROW(util::Exception, "Provide lm file name on command line.");
+  lm::ngram::ModelType model_type;
+  if (lm::ngram::RecognizeBinary(argv[1], model_type)) {
+    switch(model_type) {
+      case lm::ngram::HASH_PROBING:
+        Score<lm::ngram::ProbingModel>(argv[1]);
+        break;
+      case lm::ngram::TRIE_SORTED:
+        Score<lm::ngram::TrieModel>(argv[1]);
+        break;
+      case lm::ngram::HASH_SORTED:
+      default:
+        std::cerr << "Unrecognized kenlm model type " << model_type << std::endl;
+        abort();
+    }
+    std::cerr << "Destroyed:" << std::endl;
+    PrintStatus();
+  } else {
+    std::cerr << "Convert to binary first." << std::endl;
+    return 1;
+  }
+}
---
+++ lm/convert_for_benchmark.cc
@@ -0,0 +1,50 @@
+#include "util/file_piece.hh"
+#include "util/tokenize_piece.hh"
+#include "lm/model.hh"
+
+#include <iostream>
+
+template <class Model> void Convert(const char *name) {
+  lm::ngram::Config config;
+  config.load_method = util::LAZY;
+  Model m(name, config);
+  util::FilePiece in(0, "stdin", &std::cerr);
+  StringPiece line;
+  lm::WordIndex w;
+  try {
+    while (true) {
+      line = in.ReadLine();
+      w = m.GetVocabulary().BeginSentence();
+      if (1 != fwrite(&w, sizeof(w), 1, stdout)) UTIL_THROW(util::ErrnoException, "fwrite failed.");
+      for (util::PieceIterator<' '> i(line); i; ++i) {
+        lm::WordIndex w = m.GetVocabulary().Index(*i);
+        if (1 != fwrite(&w, sizeof(w), 1, stdout)) UTIL_THROW(util::ErrnoException, "fwrite failed.");
+      }
+      w = m.GetVocabulary().EndSentence();
+      if (1 != fwrite(&w, sizeof(w), 1, stdout)) UTIL_THROW(util::ErrnoException, "fwrite failed.");
+    }
+  } catch (const util::EndOfFileException &e) {}
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) UTIL_THROW(util::Exception, "Provide lm file name on command line.");
+  lm::ngram::ModelType model_type;
+  if (lm::ngram::RecognizeBinary(argv[1], model_type)) {
+    switch(model_type) {
+      case lm::ngram::HASH_PROBING:
+        Convert<lm::ngram::ProbingModel>(argv[1]);
+        break;
+      case lm::ngram::TRIE_SORTED:
+        Convert<lm::ngram::TrieModel>(argv[1]);
+        break;
+      case lm::ngram::HASH_SORTED:
+      default:
+        std::cerr << "Unrecognized kenlm model type " << model_type << std::endl;
+        abort();
+    }
+    return 0;
+  } else {
+    std::cerr << "Convert to binary first." << std::endl;
+    return 1;
+  }
+}
