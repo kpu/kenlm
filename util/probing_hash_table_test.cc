9a22edf81ceb5e71a3135b0d210b6d03dc484e57
Kenneth Heafield <github@kheafield.com>
2011-11-16 13:24:31 +0000

    Fix probing hash table test

--- util/probing_hash_table_test.cc
+++ util/probing_hash_table_test.cc
@@ -1,6 +1,6 @@
 #include "util/probing_hash_table.hh"
 
-#include "util/key_value_packing.hh"
+#include <stdint.h>
 
 #define BOOST_TEST_MODULE ProbingHashTableTest
 #include <boost/test/unit_test.hpp>
@@ -9,17 +9,34 @@
 namespace util {
 namespace {
 
-typedef AlignedPacking<char, uint64_t> Packing;
-typedef ProbingHashTable<Packing, boost::hash<char> > Table;
+struct Entry {
+  unsigned char key;
+  typedef unsigned char Key;
+
+  unsigned char GetKey() const {
+    return key;
+  }
+
+  uint64_t GetValue() const {
+    return value;
+  }
+
+  uint64_t value;
+};
+
+typedef ProbingHashTable<Entry, boost::hash<unsigned char> > Table;
 
 BOOST_AUTO_TEST_CASE(simple) {
   char mem[Table::Size(10, 1.2)];
   memset(mem, 0, sizeof(mem));
 
   Table table(mem, sizeof(mem));
-  Packing::ConstIterator i = Packing::ConstIterator();
+  const Entry *i = NULL;
   BOOST_CHECK(!table.Find(2, i));
-  table.Insert(Packing::Make(3, 328920));
+  Entry to_ins;
+  to_ins.key = 3;
+  to_ins.value = 328920;
+  table.Insert(to_ins);
   BOOST_REQUIRE(table.Find(3, i));
   BOOST_CHECK_EQUAL(3, i->GetKey());
   BOOST_CHECK_EQUAL(static_cast<uint64_t>(328920), i->GetValue());
