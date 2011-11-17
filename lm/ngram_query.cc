ac5c57329a51ea587bcc26d5bbf20d91fbfc3fd9
Kenneth Heafield <github@kheafield.com>
2011-11-17 15:47:46 +0000

    try and print around query

--- lm/ngram_query.cc
+++ lm/ngram_query.cc
@@ -94,34 +94,39 @@ int main(int argc, char *argv[]) {
     std::cerr << "Input is wrapped in <s> and </s> unless null is passed." << std::endl;
     return 1;
   }
-  bool sentence_context = (argc == 2);
-  lm::ngram::ModelType model_type;
-  if (lm::ngram::RecognizeBinary(argv[1], model_type)) {
-    switch(model_type) {
-      case lm::ngram::HASH_PROBING:
-        Query<lm::ngram::ProbingModel>(argv[1], sentence_context);
-        break;
-      case lm::ngram::TRIE_SORTED:
-        Query<lm::ngram::TrieModel>(argv[1], sentence_context);
-        break;
-      case lm::ngram::QUANT_TRIE_SORTED:
-        Query<lm::ngram::QuantTrieModel>(argv[1], sentence_context);
-        break;
-      case lm::ngram::ARRAY_TRIE_SORTED:
-        Query<lm::ngram::ArrayTrieModel>(argv[1], sentence_context);
-        break;
-      case lm::ngram::QUANT_ARRAY_TRIE_SORTED:
-        Query<lm::ngram::QuantArrayTrieModel>(argv[1], sentence_context);
-        break;
-      case lm::ngram::HASH_SORTED:
-      default:
-        std::cerr << "Unrecognized kenlm model type " << model_type << std::endl;
-        abort();
+  try {
+    bool sentence_context = (argc == 2);
+    lm::ngram::ModelType model_type;
+    if (lm::ngram::RecognizeBinary(argv[1], model_type)) {
+      switch(model_type) {
+        case lm::ngram::HASH_PROBING:
+          Query<lm::ngram::ProbingModel>(argv[1], sentence_context);
+          break;
+        case lm::ngram::TRIE_SORTED:
+          Query<lm::ngram::TrieModel>(argv[1], sentence_context);
+          break;
+        case lm::ngram::QUANT_TRIE_SORTED:
+          Query<lm::ngram::QuantTrieModel>(argv[1], sentence_context);
+          break;
+        case lm::ngram::ARRAY_TRIE_SORTED:
+          Query<lm::ngram::ArrayTrieModel>(argv[1], sentence_context);
+          break;
+        case lm::ngram::QUANT_ARRAY_TRIE_SORTED:
+          Query<lm::ngram::QuantArrayTrieModel>(argv[1], sentence_context);
+          break;
+        case lm::ngram::HASH_SORTED:
+        default:
+          std::cerr << "Unrecognized kenlm model type " << model_type << std::endl;
+          abort();
+      }
+    } else {
+      Query<lm::ngram::ProbingModel>(argv[1], sentence_context);
     }
-  } else {
-    Query<lm::ngram::ProbingModel>(argv[1], sentence_context);
-  }
 
-  PrintUsage("Total time including destruction:\n");
+    PrintUsage("Total time including destruction:\n");
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
   return 0;
 }
