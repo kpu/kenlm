cd0a3607f43776d19eb33ad73dc2981fc0119130
Kenneth Heafield <github@kheafield.com>
2012-06-28 09:58:06 -0400

    Merge probing hash table test fix for clang

--- util/probing_hash_table_test.cc
+++ util/probing_hash_table_test.cc
@@ -1,10 +1,11 @@
 #include "util/probing_hash_table.hh"
 
-#include <stdint.h>
-
 #define BOOST_TEST_MODULE ProbingHashTableTest
 #include <boost/test/unit_test.hpp>
+#include <boost/scoped_array.hpp>
 #include <boost/functional/hash.hpp>
+
+#include <stdint.h>
 
 namespace util {
 namespace {
@@ -27,10 +28,9 @@ struct Entry {
 typedef ProbingHashTable<Entry, boost::hash<unsigned char> > Table;
 
 BOOST_AUTO_TEST_CASE(simple) {
-  char mem[Table::Size(10, 1.2)];
-  memset(mem, 0, sizeof(mem));
+  boost::scoped_array<char> mem(new char[Table::Size(10, 1.2)]);
 
-  Table table(mem, sizeof(mem));
+  Table table(mem.get(), Table::Size(10, 1.2));
   const Entry *i = NULL;
   BOOST_CHECK(!table.Find(2, i));
   Entry to_ins;
