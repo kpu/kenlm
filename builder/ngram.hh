5c7a2bb5018d9850c44787a216d159a5c539c07c
Ivan Pouzyrevsky <sandello@gmail.com>
2012-09-04 17:27:04 +0000

    Introduce CountedNGram definition

--- builder/ngram.hh
+++ builder/ngram.hh
@@ -6,7 +6,12 @@
 namespace builder {
 
 template <unsigned N> struct NGram {
+  static const unsigned n = N;
   lm::WordIndex w[N];
+};
+
+template <unsigned N> struct CountedNGram : NGram<N> {
+  uint64_t count;
 };
 
 } // namespace builder
