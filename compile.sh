6489a9122eb5ceeed696756b9d900e7d7aac4d67
Kenneth Heafield <github@kheafield.com>
2012-10-31 14:57:08 +0000

    Build fixes

--- compile.sh
+++ compile.sh
@@ -6,9 +6,11 @@
 rm {lm,util}/*.o 2>/dev/null
 set -e
 
-for i in util/{bit_packing,ersatz_progress,exception,file,file_piece,murmur_hash,mmap,read_compressed,string_piece,usage} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,trie,trie_sort,value_build,virtual_interface,vocab}; do
-  g++ -I. -O3 -DNDEBUG -DHAVE_ZLIB $CXXFLAGS -c $i.cc -o $i.o
+CXXFLAGS="-I. -O3 -DNDEBUG -DHAVE_ZLIB -DKENLM_MAX_ORDER=6 $CXXFLAGS"
+
+for i in util/{bit_packing,ersatz_progress,exception,file,file_piece,murmur_hash,mmap,pool,read_compressed,string_piece,usage} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,trie,trie_sort,value_build,virtual_interface,vocab}; do
+  g++ $CXXFLAGS -c $i.cc -o $i.o
 done
-g++ -I. -O3 -DNDEBUG $CXXFLAGS lm/build_binary.cc {lm,util}/*.o -lz -o build_binary
-g++ -I. -O3 -DNDEBUG $CXXFLAGS lm/ngram_query.cc {lm,util}/*.o -lz -o query
-g++ -I. -O3 -DNDEBUG $CXXFLAGS lm/max_order.cc -o kenlm_max_order
+g++ $CXXFLAGS lm/build_binary.cc {lm,util}/*.o -lz -o build_binary
+g++ $CXXFLAGS lm/ngram_query.cc {lm,util}/*.o -lz -o query
+g++ $CXXFLAGS lm/max_order.cc -o kenlm_max_order
