05004810957e5b0487e9324bc3741006d3bf1bbb
Kenneth Heafield <github@kheafield.com>
2012-11-20 13:28:33 +0000

    Stream link

---
+++ util/stream/stream.hh
@@ -0,0 +1,50 @@
+#ifndef UTIL_STREAM_STREAM__
+#define UTIL_STREAM_STREAM__
+
+#include "util/stream/chain.hh"
+
+#include <boost/noncopyable.hpp>
+
+#include <assert.h>
+#include <stdint.h>
+
+namespace util {
+namespace stream {
+
+class Stream : boost::noncopyable {
+  public:
+    explicit Stream(const ChainPosition &position) : 
+        entry_size_(position.GetChain().EntrySize()), block_it_(position) {
+      current_ = static_cast<uint8_t*>(block_it_->Get());
+      end_ = current_ + block_it_->ValidSize();
+    }
+
+    operator bool() const { return current_ != NULL; }
+    bool operator!() const { return current_ == NULL; }
+
+    const void *Get() const { return current_; }
+    void *Get() { return current_; }
+    
+    Stream &operator++() {
+      assert(*this);
+      assert(current_ < end_);
+      current_ += entry_size_;
+      while (current_ == end_ && current_) {
+        ++block_it_;
+        current_ = static_cast<uint8_t*>(block_it_->Get());
+        end_ = current_ + block_it_->ValidSize();
+      }  
+      return *this;
+    }
+
+  private:
+    uint8_t *current_, *end_;
+
+    const std::size_t entry_size_;
+
+    Link block_it_;
+};
+ 
+} // namespace stream
+} // namespace util
+#endif // UTIL_STREAM_STREAM__
---
+++ util/stream/stream_test.cc
@@ -0,0 +1,35 @@
+#include "util/stream/io.hh"
+#include "util/stream/stream.hh"
+
+#define BOOST_TEST_MODULE StreamTest
+#include <boost/test/unit_test.hpp>
+
+#include <unistd.h>
+
+namespace util { namespace stream { namespace {
+
+BOOST_AUTO_TEST_CASE(StreamTest) {
+  util::TempMaker temps("io_test_temp");
+
+  scoped_fd in(temps.Make());
+  for (uint64_t i = 0; i < 100000; ++i) {
+    WriteOrThrow(in.get(), &i, sizeof(uint64_t));
+  }
+  SeekOrThrow(in.get(), 0);
+
+  ChainConfig config;
+  config.entry_size = 8;
+  config.block_size = 30;
+  config.block_count = 12;
+  config.queue_length = 2;
+
+  Chain chain(config);
+  ReadThread read(chain.Between(), in.get());
+  uint64_t i = 0;
+  for (Stream s(chain.Last()); s; ++s, ++i) {
+    BOOST_CHECK_EQUAL(i, *static_cast<const uint64_t*>(s.Get()));
+  }
+  BOOST_CHECK_EQUAL(100000, i);
+}
+
+}}} // namespaces
