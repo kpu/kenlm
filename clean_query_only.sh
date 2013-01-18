dffafbf9d36bbe2cab30cb7a752e982b2641fb3d
Kenneth Heafield <github@kheafield.com>
2013-01-18 17:04:40 +0000

    Update simple build scripts to double-conversion

--- clean_query_only.sh
+++ clean_query_only.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf */*.o query kenlm_max_order build_binary */*_test lm/test.binary* lm/test.arpa?????? util/file_piece.cc.gz
+rm -rf {lm,util,util/double-conversion}/*.o query kenlm_max_order build_binary */*_test lm/test.binary* lm/test.arpa?????? util/file_piece.cc.gz
