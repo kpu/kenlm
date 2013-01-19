fcf54c454d15b057d21d5d911f922410633ed437
Kenneth Heafield <github@kheafield.com>
2013-01-19 12:30:21 +0000

    Make compile_query_only also go to bin.

--- compile_query_only.sh
+++ compile_query_only.sh
@@ -13,6 +13,7 @@ CXXFLAGS="-I. -O3 -DNDEBUG -DHAVE_ZLIB -DKENLM_MAX_ORDER=6 $CXXFLAGS"
 for i in util/{bit_packing,ersatz_progress,exception,file,file_piece,murmur_hash,mmap,pool,read_compressed,scoped,string_piece,usage} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,sizes,trie,trie_sort,value_build,virtual_interface,vocab} util/double-conversion/{bignum,bignum-dtoa,cached-powers,diy-fp,double-conversion,fast-dtoa,fixed-dtoa,strtod}; do
   g++ $CXXFLAGS -c $i.cc -o $i.o
 done
-g++ $CXXFLAGS lm/build_binary.cc {lm,util,util/double-conversion}/*.o -lz -o build_binary
-g++ $CXXFLAGS lm/ngram_query.cc {lm,util,util/double-conversion}/*.o -lz -o query
-g++ $CXXFLAGS lm/max_order.cc -o kenlm_max_order
+mkdir -p bin
+g++ $CXXFLAGS lm/build_binary.cc {lm,util,util/double-conversion}/*.o -lz -o bin/build_binary
+g++ $CXXFLAGS lm/ngram_query.cc {lm,util,util/double-conversion}/*.o -lz -o bin/query
+g++ $CXXFLAGS lm/max_order.cc -o bin/kenlm_max_order
