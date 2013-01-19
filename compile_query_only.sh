7a0d82450f5578037ff4169be6bc14b31a3a8845
Kenneth Heafield <github@kheafield.com>
2013-01-19 16:03:10 +0000

    Default to no zlib

--- compile_query_only.sh
+++ compile_query_only.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-#This is just an example compilation.  You should integrate these files into your build system.  Boost jam is provided and preferred.  
-#If your code uses ICU, define HAVE_ICU
-#I use zlib by default.  If you don't want to depend on zlib, remove -DHAVE_ZLIB and -lz.  
+#This is just an example compilation.  You should integrate these files into your build system.  Boost jam is provided and preferred.
 
 echo Note: You must use ./bjam if you want language model estimation or filtering 1>&2
 
 rm {lm,util}/*.o 2>/dev/null
 set -e
 
-CXXFLAGS="-I. -O3 -DNDEBUG -DHAVE_ZLIB -DKENLM_MAX_ORDER=6 $CXXFLAGS"
+CXXFLAGS="-I. -O3 -DNDEBUG -DKENLM_MAX_ORDER=6 $CXXFLAGS"
 
 for i in util/{bit_packing,ersatz_progress,exception,file,file_piece,murmur_hash,mmap,pool,read_compressed,scoped,string_piece,usage} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,sizes,trie,trie_sort,value_build,virtual_interface,vocab} util/double-conversion/{bignum,bignum-dtoa,cached-powers,diy-fp,double-conversion,fast-dtoa,fixed-dtoa,strtod}; do
   g++ $CXXFLAGS -c $i.cc -o $i.o
 done
 mkdir -p bin
-g++ $CXXFLAGS lm/build_binary.cc {lm,util,util/double-conversion}/*.o -lz -o bin/build_binary
-g++ $CXXFLAGS lm/ngram_query.cc {lm,util,util/double-conversion}/*.o -lz -o bin/query
+g++ $CXXFLAGS lm/build_binary.cc {lm,util,util/double-conversion}/*.o -o bin/build_binary
+g++ $CXXFLAGS lm/ngram_query.cc {lm,util,util/double-conversion}/*.o -o bin/query
 g++ $CXXFLAGS lm/max_order.cc -o bin/kenlm_max_order
