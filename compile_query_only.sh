8d4f78c82e4468dff267b8ae6efd7388069f2a7a
Kenneth Heafield <github@kheafield.com>
2013-01-20 13:20:25 +0000

    Globs for compile_query_only.

--- compile_query_only.sh
+++ compile_query_only.sh
@@ -8,10 +8,16 @@ set -e
 
 CXXFLAGS="-I. -O3 -DNDEBUG -DKENLM_MAX_ORDER=6 $CXXFLAGS"
 
-for i in util/{bit_packing,ersatz_progress,exception,file,file_piece,murmur_hash,mmap,pool,read_compressed,scoped,string_piece,usage} lm/{bhiksha,binary_format,config,lm_exception,model,quantize,read_arpa,search_hashed,search_trie,sizes,trie,trie_sort,value_build,virtual_interface,vocab} util/double-conversion/{bignum,bignum-dtoa,cached-powers,diy-fp,double-conversion,fast-dtoa,fixed-dtoa,strtod}; do
-  g++ $CXXFLAGS -c $i.cc -o $i.o
+#Grab all cc files in these directories except those ending in test.cc or main.cc
+objects=""
+for i in util/double-conversion/*.cc util/*.cc lm/*.cc; do
+  if [ "${i%test.cc}" == "$i" ] && [ "${i%main.cc}" == "$i" ]; then
+    g++ $CXXFLAGS -c $i -o ${i%.cc}.o
+    objects="$objects ${i%.cc}.o"
+  fi
 done
+
 mkdir -p bin
-g++ $CXXFLAGS lm/build_binary_main.cc {lm,util,util/double-conversion}/*.o -o bin/build_binary
-g++ $CXXFLAGS lm/query_main.cc {lm,util,util/double-conversion}/*.o -o bin/query
+g++ $CXXFLAGS lm/build_binary_main.cc $objects -o bin/build_binary
+g++ $CXXFLAGS lm/query_main.cc $objects -o bin/query
 g++ $CXXFLAGS lm/kenlm_max_order_main.cc -o bin/kenlm_max_order
