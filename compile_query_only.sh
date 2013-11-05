cb84ab87d6133405ea8217f91e7ccbbfea46c1ee
Kenneth Heafield <github@kheafield.com>
2013-11-04 19:21:14 -0800

    Add compilation for NPLM in compile_query_only.sh

--- compile_query_only.sh
+++ compile_query_only.sh
@@ -7,12 +7,19 @@ rm {lm,util}/*.o 2>/dev/null
 set -e
 
 CXX=${CXX:-g++}
+
 CXXFLAGS+=" -I. -O3 -DNDEBUG -DKENLM_MAX_ORDER=6"
+
+#If this fails for you, consider using bjam.
+if [ ${#NPLM} != 0 ]; then
+  CXXFLAGS+=" -DHAVE_NPLM -lneuralLM -L$NPLM/src -I$NPLM/src -lboost_thread-mt -fopenmp"
+  ADDED_PATHS="lm/wrappers/*.cc"
+fi
 echo 'Compiling with '$CXX $CXXFLAGS
 
 #Grab all cc files in these directories except those ending in test.cc or main.cc
 objects=""
-for i in util/double-conversion/*.cc util/*.cc lm/*.cc; do
+for i in util/double-conversion/*.cc util/*.cc lm/*.cc $ADDED_PATHS; do
   if [ "${i%test.cc}" == "$i" ] && [ "${i%main.cc}" == "$i" ]; then
     $CXX $CXXFLAGS -c $i -o ${i%.cc}.o
     objects="$objects ${i%.cc}.o"
