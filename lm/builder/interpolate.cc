afea0b46415ffe0deca7644f5ebe40b6925c1f65
Kenneth Heafield <github@kheafield.com>
2014-02-15 15:44:16 -0800

    Write q files

--- lm/builder/interpolate.cc
+++ lm/builder/interpolate.cc
@@ -7,6 +7,9 @@
 #include "lm/builder/sort.hh"
 #include "lm/lm_exception.hh"
 
+#include <iostream>
+#include <numeric>
+
 #include <assert.h>
 
 namespace lm { namespace builder {
@@ -14,17 +17,33 @@ namespace {
 
 class Callback {
   public:
-    Callback(float uniform_prob, Binarize &binarize, const ChainPositions &backoffs) 
-      : backoffs_(backoffs.size()), probs_(backoffs.size() + 2), binarize_(binarize) {
+    Callback(float uniform_prob, Binarize &binarize, const ChainPositions &backoffs)
+      : full_backoffs_(backoffs.size()), q_files_(backoffs.size() + 1), q_chain_(backoffs.size() + 1), q_out_(backoffs.size() + 1), probs_(backoffs.size() + 2 /* zeroton has uniform prob */), q_delta_(backoffs.size() + 1), binarize_(binarize) {
+      // This stays forever as the zeroton.
       probs_[0] = uniform_prob;
       for (std::size_t i = 0; i < backoffs.size(); ++i) {
-        backoffs_.push_back(backoffs[i]);
+        full_backoffs_.push_back(backoffs[i]);
+      }
+      util::stream::ChainConfig write_qs;
+      write_qs.entry_size = 4;
+      write_qs.total_memory = 1048576;
+      write_qs.block_count = 2;
+      for (std::size_t i = 0; i < backoffs.size() + 1; ++i) {
+        std::string file("q");
+        file += boost::lexical_cast<std::string>(i + 1);
+        q_files_.push_back(util::CreateOrThrow(file.c_str()));
+        q_chain_.push_back(write_qs);
+        q_out_.push_back(q_chain_.back().Add());
+        q_chain_.back() >> util::stream::WriteAndRecycle(q_files_.back().get());
       }
     }
 
     ~Callback() {
-      for (std::size_t i = 0; i < backoffs_.size(); ++i) {
-        if (backoffs_[i]) {
+      for (std::size_t i = 0; i < q_chain_.size(); ++i) {
+        q_out_[i].Poison();
+      }
+      for (std::size_t i = 0; i < full_backoffs_.size(); ++i) {
+        if (full_backoffs_[i]) {
           std::cerr << "Backoffs do not match for order " << (i + 1) << std::endl;
           //abort();
         }
@@ -33,27 +52,46 @@ class Callback {
 
     void Enter(unsigned order_minus_1, NGram &gram) {
       Payload &pay = gram.Value();
+      // q delta subtracts the context's backoff
+      if (order_minus_1) {
+        q_delta_[order_minus_1] = q_delta_[order_minus_1 - 1] / pay.uninterp.gamma;
+      } else {
+        q_delta_[0] = 1.0;
+      }
+
       pay.complete.prob = pay.uninterp.prob + pay.uninterp.gamma * probs_[order_minus_1];
+      float actual_prob = pay.complete.prob;
       probs_[order_minus_1 + 1] = pay.complete.prob;
       pay.complete.prob = log10(pay.complete.prob);
       // TODO: this is a hack to skip n-grams that don't appear as context.  Pruning will require some different handling.  
-      if (order_minus_1 < backoffs_.size() && *(gram.end() - 1) != kUNK && *(gram.end() - 1) != binarize_.EndSentence()) {
-        pay.complete.backoff = log10(*static_cast<const float*>(backoffs_[order_minus_1].Get()));
+      if (order_minus_1 < full_backoffs_.size() && *(gram.end() - 1) != kUNK && *(gram.end() - 1) != binarize_.EndSentence()) {
+        float full_backoff = *static_cast<const float*>(full_backoffs_[order_minus_1].Get());
+        q_delta_[order_minus_1] *= full_backoff;
+        pay.complete.backoff = log10(full_backoff);
         ngram::SetExtension(pay.complete.backoff);
-        ++backoffs_[order_minus_1];
+        ++full_backoffs_[order_minus_1];
       } else {
         // Not a context.  
         pay.complete.backoff = ngram::kNoExtensionBackoff;
       }
+      // Write the q value ot to a stream.
+      *static_cast<float*>(q_out_[order_minus_1].Get()) = q_delta_[order_minus_1] * actual_prob;
+      ++q_out_[order_minus_1];
       binarize_.Enter(order_minus_1, gram);
     }
 
    // void Exit(unsigned, const NGram &) const {}
 
   private:
-    FixedArray<util::stream::Stream> backoffs_;
+    FixedArray<util::stream::Stream> full_backoffs_;
 
-    std::vector<float> probs_;
+    FixedArray<util::scoped_fd> q_files_;
+
+    FixedArray<util::stream::Chain> q_chain_;
+
+    FixedArray<util::stream::Stream> q_out_;
+
+    std::vector<float> probs_, q_delta_;
 
     Binarize &binarize_;
 };
