929d948e04ef8cf633be9b1f2762b02c414cdf2a
Kenneth Heafield <github@kheafield.com>
2014-02-22 09:50:37 -0800

    Add option for n-gram length

--- lm/builder/dump_counts_main.cc
+++ lm/builder/dump_counts_main.cc
@@ -2,14 +2,17 @@
 #include "util/file.hh"
 #include "util/stream/chain.hh"
 #include "util/stream/io.hh"
+
 #include <iostream>
 
+#include <boost/lexical_cast.hpp>
+
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Expected null-delimited vocab file." << std::endl;
+  if (argc != 3) {
+    std::cerr << "Expected null-delimited vocab file and order." << std::endl;
     return 1;
   }
   util::scoped_fd vocab(util::OpenReadOrThrow(argv[1]));
   lm::builder::VocabReconstitute reconstitute(vocab.get());
-  util::stream::Chain(util::stream::ChainConfig(5*4+8, 1, 4096)) >> util::stream::Read(0) >> lm::builder::Print<uint64_t>(reconstitute, std::cout);
+  util::stream::Chain(util::stream::ChainConfig(boost::lexical_cast<unsigned int>(argv[2])*4+8, 1, 4096)) >> util::stream::Read(0) >> lm::builder::Print<uint64_t>(reconstitute, std::cout);
 }
