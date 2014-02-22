5d887a5a31d72d7620ae514796ea5f473582c4c5
Kenneth Heafield <github@kheafield.com>
2014-02-22 17:12:15 +0000

    Fix merge compilation

--- lm/builder/merge_main.cc
+++ lm/builder/merge_main.cc
@@ -6,7 +6,7 @@ using namespace lm::builder;
 
 int main(int argc, char *argv[]) {
   util::stream::FileMergingReader<SuffixOrder, AddCombiner> reader(SuffixOrder(5), AddCombiner());
-  for (unsigned int i = 1; i < argc, ++i) {
+  for (unsigned int i = 1; i < argc; ++i) {
     reader.Add(argv[i]);
   }
   util::stream::Chain(util::stream::ChainConfig(5*4 + 8, 2, 1048576)) >> boost::ref(reader) >> util::stream::Write(1);
