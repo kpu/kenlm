1cb07a22b1582f3f34796602714b0c7734560e48
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2014-04-08 00:06:15 +0100

    took nicer command line options from branch "pruning"

--- lm/builder/lmplz_main.cc
+++ lm/builder/lmplz_main.cc
@@ -26,6 +26,46 @@ boost::program_options::typed_value<std::string> *SizeOption(std::size_t &to, co
   return boost::program_options::value<std::string>()->notifier(SizeNotify(to))->default_value(default_value);
 }
 
+// Parse and validate pruning thresholds then return vector of threshold counts
+// for each n-grams order.
+std::vector<uint64_t> ParsePruning(const std::vector<std::string> &param, std::size_t order) {
+  // convert to vector of integers
+  std::vector<uint64_t> prune_thresholds;
+  prune_thresholds.reserve(order);
+  std::cerr << "Pruning ";
+  for (std::vector<std::string>::const_iterator it(param.begin()); it != param.end(); ++it) {
+    try {
+      prune_thresholds.push_back(boost::lexical_cast<uint64_t>(*it));
+    } catch(const boost::bad_lexical_cast &) {
+      UTIL_THROW(util::Exception, "Bad pruning threshold " << *it);
+    }
+  }
+
+  // Fill with zeros by default.
+  if (prune_thresholds.empty()) {
+    prune_thresholds.resize(order, 0);
+    return prune_thresholds;
+  }
+
+  // validate pruning threshold if specified
+  // throw if each n-gram order has not  threshold specified
+  UTIL_THROW_IF(prune_thresholds.size() > order, util::Exception, "You specified pruning thresholds for orders 1 through " << prune_thresholds.size() << " but the model only has order " << order);
+  // threshold for unigram can only be 0 (no pruning)
+  UTIL_THROW_IF(prune_thresholds[0] != 0, util::Exception, "Unigram pruning is not implemented, so the first pruning threshold must be 0.");
+
+  // check if threshold are not in decreasing order
+  uint64_t lower_threshold = 0;
+  for (std::vector<uint64_t>::iterator it = prune_thresholds.begin(); it != prune_thresholds.end(); ++it) {
+    UTIL_THROW_IF(lower_threshold > *it, util::Exception, "Pruning thresholds should be in non-decreasing order.  Otherwise substrings would be removed, which is bad for query-time data structures.");
+    lower_threshold = *it;
+  }
+
+  // Pad to all orders using the last value.
+  prune_thresholds.resize(order, prune_thresholds.back());
+  return prune_thresholds;
+}
+
+
 } // namespace
 
 int main(int argc, char *argv[]) {
@@ -35,6 +75,8 @@ int main(int argc, char *argv[]) {
     lm::builder::PipelineConfig pipeline;
 
     std::string text, arpa;
+    std::vector<std::string> pruning;
+
 
     options.add_options()
       ("order,o", po::value<std::size_t>(&pipeline.order)
@@ -53,7 +95,7 @@ int main(int argc, char *argv[]) {
       ("verbose_header", po::bool_switch(&pipeline.verbose_header), "Add a verbose header to the ARPA file that includes information such as token count, smoothing type, etc.")
       ("text", po::value<std::string>(&text), "Read text from a file instead of stdin")
       ("arpa", po::value<std::string>(&arpa), "Write ARPA to a file instead of stdout")
-      ("prune_thresholds,P", po::value<std::vector<uint64_t> >(&pipeline.prune_thresholds), "Prune n-grams of count equal to or lower than threshold. 0 means no pruning");
+      ("prune,P", po::value<std::vector<std::string> >(&pruning)->multitoken(), "Prune n-grams with count less than or equal to the given threshold.  Specify one value for each order i.e. 0 0 1 to prune singleton trigrams and above.  The sequence of values must be non-decreasing and the last value applies to any remaining orders.  Unigram pruning is not implemented, so the first value must be zero.  Default is to not prune, which is equivalent to -prune 0.");
     if (argc == 1) {
       std::cerr << 
         "Builds unpruned language models with modified Kneser-Ney smoothing.\n\n"
@@ -80,38 +122,6 @@ int main(int argc, char *argv[]) {
     po::store(po::parse_command_line(argc, argv, options), vm);
     po::notify(vm);
 
-    //std::cerr << "vector: " << pipeline.counts_threshold.size() << std::endl;
-    // validate pruning threshold if specified
-    if (pipeline.prune_thresholds.size() > 0) {
-       // chekc if each n-gram order has threshold specified
-       if (pipeline.prune_thresholds.size() != pipeline.order) {
-           std::cerr << 
-            "Incorrect use of pruning option. Set pruning threshold for each order n-grams.\n"
-            << "Threshold counts set: " << pipeline.prune_thresholds.size() << std::endl
-            << "Threshold counts expected: " << pipeline.order << std::endl; 
-           return 1;
-       }
-       // threshold for unigram can only be 0 (no pruning)
-       if (pipeline.prune_thresholds[0] != 0) {
-        std::cerr << "You are not allowed to prune unigrams. Set counts_threshold to 0 fro unigrams."
-            "Currently you are setting " << pipeline.prune_thresholds[0] << std::endl;
-        return 1;
-       }
-       // check if threshold are not in decreasing order
-       uint64_t lower_threshold = 0;
-       for (std::vector<uint64_t>::iterator it = pipeline.prune_thresholds.begin(); it != pipeline.prune_thresholds.end(); ++it)
-           if (lower_threshold > *it) {
-               std::cerr << "Do not specify pruning threshold counts in decreasing order\n"
-              "Pruning threshold for lower-order n-grams must be equal to or lower than for higher-order n-grams" << std::endl;
-               return 1;
-           } else lower_threshold = *it;
-    } else {
-       // pruning is not specified, so set threshold to 0 for all order n-grams
-       pipeline.prune_thresholds.reserve(pipeline.order);
-       for (std::size_t i = 0; i < pipeline.order; ++i)
-            pipeline.prune_thresholds.push_back(0);
-    }
-
     // required() appeared in Boost 1.42.0.
 #if BOOST_VERSION < 104200
     if (!vm.count("order")) {
@@ -120,6 +130,9 @@ int main(int argc, char *argv[]) {
     }
 #endif
 
+    // parse pruning thresholds.  These depend on order, so it is not done as a notifier.
+    pipeline.prune_thresholds = ParsePruning(pruning, pipeline.order);
+    
     util::NormalizeTempPrefix(pipeline.sort.temp_prefix);
 
     lm::builder::InitialProbabilitiesConfig &initial = pipeline.initial_probs;
