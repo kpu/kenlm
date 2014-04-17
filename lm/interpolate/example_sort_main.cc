5b7c50bbddcf8d5065750cef2dc033d6084fe8d4
Lane Schwartz <dowobeha@gmail.com>
2014-04-17 14:20:00 -0400

    Pulled out constants and used system defined constants for STDIN and STDOUT

--- lm/interpolate/example_sort_main.cc
+++ lm/interpolate/example_sort_main.cc
@@ -5,28 +5,34 @@
 #include "lm/vocab.hh"
 #include "util/file.hh"
 
+#include <unistd.h>
+
 int main() {
+  const std::size_t ONE_GB = 1 << 30;
+  const std::size_t SIXTY_FOUR_MB = 1 << 26;
+  
   util::scoped_fd vocab_file(util::MakeTemp("/tmp/"));
   std::vector<uint64_t> counts;
   util::stream::Chains chains;
   {
     // Use consistent vocab ids across models.
     lm::ngram::GrowableVocab vocab(10, vocab_file.get());
-    lm::interpolate::ARPAToStream reader(0, vocab);
+    lm::interpolate::ARPAToStream reader(STDIN_FILENO, vocab);
     counts = reader.Counts();
 
     // Configure a chain for each order.  TODO: extract chain balance heuristics from lm/builder/pipeline.cc
     chains.Init(reader.Order());
     for (std::size_t i = 0; i < reader.Order(); ++i) {
-      chains.push_back(util::stream::ChainConfig(lm::builder::NGram::TotalSize(i + 1), 2, 1 << 30 /* 1 GB */));
+      chains.push_back(util::stream::ChainConfig(lm::builder::NGram::TotalSize(i + 1), 2, ONE_GB));
     }
     // Normally >> copies then runs so inline >> works.  But here we want a ref.
     chains >> boost::ref(reader);
 
     util::stream::SortConfig sort_config;
-    sort_config.temp_prefix = "/tmp/";
-    sort_config.buffer_size = 1 << 26;
-    sort_config.total_memory = 1 << 30;
+    sort_config.temp_prefix  = "/tmp/";
+    sort_config.buffer_size  = SIXTY_FOUR_MB;
+    sort_config.total_memory = ONE_GB;
+    
     // Parallel sorts across orders (though somewhat limited because ARPA files are not being read in parallel across orders)
     lm::builder::Sorts<lm::builder::SuffixOrder> sorts(reader.Order());
     for (std::size_t i = 0; i < reader.Order(); ++i) {
@@ -42,6 +48,6 @@ int main() {
   }
 
   lm::builder::VocabReconstitute reconstitute(vocab_file.get());
-  chains >> lm::builder::PrintARPA(reconstitute, counts, NULL, 1);
+  chains >> lm::builder::PrintARPA(reconstitute, counts, NULL, STDOUT_FILENO);
   chains.Wait(true);
 }
