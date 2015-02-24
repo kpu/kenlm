959f195854d1cd7bc73e76adbcc29ad52536757d
Kenneth Heafield <github@kheafield.com>
2015-02-24 08:20:34 -0500

    Explain --discount_fallback in error message

--- lm/builder/adjust_counts.cc
+++ lm/builder/adjust_counts.cc
@@ -48,7 +48,8 @@ class StatCollector {
             // TODO: Specialize error message for j == 3, meaning 3+
             UTIL_THROW_IF(s.n[j] == 0, BadDiscountException, "Could not calculate Kneser-Ney discounts for "
                 << (i+1) << "-grams with adjusted count " << (j+1) << " because we didn't observe any "
-                << (i+1) << "-grams with adjusted count " << j << "; Is this small or artificial data?");
+                << (i+1) << "-grams with adjusted count " << j << "; Is this small or artificial data?\n"
+                << "Try deduplicating the input.  To override this error for e.g. a class-based model, rerun with --discount_fallback\n");
           }
 
           // See equation (26) in Chen and Goodman.
