3007e05a5925269266ceb7f4b12a6dfc0407d7f9
Kenneth Heafield <github@kheafield.com>
2015-05-18 15:36:43 -0400

    Pretty sure this was meant to be resize not reserve

--- lm/interpolate/universal_vocab.cc
+++ lm/interpolate/universal_vocab.cc
@@ -6,7 +6,7 @@ namespace interpolate {
 UniversalVocab::UniversalVocab(const std::vector<WordIndex>& model_max_idx) {
   model_index_map_.resize(model_max_idx.size());
   for (size_t i = 0; i < model_max_idx.size(); ++i) {
-    model_index_map_[i].reserve(model_max_idx[i]);
+    model_index_map_[i].resize(model_max_idx[i]);
   }
 }
 
