7f1723a490d5d7bd09edad4a87424b34f7203e55
Grant Erdmann <erdmangd@scream.lab>
2015-05-28 10:31:38 -0400

    Can allow extrapolation and negative weights. Better output for the user.

--- lm/interpolate/train_params_main.cc
+++ lm/interpolate/train_params_main.cc
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <iomanip>
 
 #include <boost/program_options.hpp>
 #include <boost/version.hpp>
@@ -75,7 +76,9 @@ void train_params(
     const std::vector<Model *>& models) {
   using namespace std;
 
-  const int ITERATIONS = 10;
+  bool AllowExtrapolation = true;
+  bool AllowNegativeParams = true;
+  const int ITERATIONS = 20;
   const int nlambdas = models.size() + (HAS_BIAS ? 1 : 0); // bias + #models
   DVector params = DVector::Constant(nlambdas,1.0/nlambdas); // initialize to sum to 1
   DMatrix N = DMatrix::Constant(nlambdas,nlambdas-1, -1.0/sqrt((nlambdas-1)*(nlambdas-1)+nlambdas-1.0));
@@ -84,17 +87,46 @@ void train_params(
   // N is nullspace matrix, each column sums to zero
   //cerr << N << endl;
 
-  cerr << "ITERATION 0/" << ITERATIONS << endl;
-  cerr << params << endl;
-
   vector<DVector> paramhistory;
-  vector<DVector> deltaparamhistory;
-  vector<double> pplhistory;
-  vector<double> fractionhistory;
+
+  double bestppl=0.0; // best recorded ppl
+  DVector bestgrad = DVector::Zero(nlambdas); // corresp. gradient, feasible direction
+  DVector bestparams = DVector::Zero(nlambdas); // corresp. weights
+  double stepsize = 1; // dist. from bestparams to params
+  double maxbestgradstep=0.0; // max feasible step in grad. direction
+  double minstepsize=1.0e-12; // convergence criterion
+  int context_size=5; // (context_size+1)-grams considered in perplexity
+  double stepdecreasefactor=0.1; // if step unsuccessful
+
+  cerr << "++ Parameter training ++" << endl;
+  if (AllowExtrapolation)
+    cerr << "Allowing extrapolation (sharpening and flattening of individual LM distributions)" << endl;
+  else
+    cerr << "Interpolating only (not sharpening or flattening individual LM distributions)" << endl;
+  if (AllowNegativeParams)
+    cerr << "Allowing negative parameters\n" <<
+      " (more general but slow and rarely useful\n" <<
+      "  -LM with negative weight has probability rankings reversed and is weighted higher than all LMs with positive weights)" << endl;
+  else
+    cerr << "Not allowing negative parameters (mild assumption, and faster)" << endl;
+  cerr << " Maximum number of iterations: " << ITERATIONS << endl;
+  cerr << " Minimum step size: " << minstepsize << endl;
+  cerr << " Perplexity computed with " << context_size+1 << "-grams" << endl;
+
+  if ((!AllowExtrapolation) && (nlambdas==1))
+  {
+    // Nothing to optimize. One parameter, and it sums to one.
+    cerr << "Training complete. Best weights:" << endl;
+    cerr << setprecision(16) << bestparams << endl;
+    return;
+  }
 
   for (int iter = 0; iter < ITERATIONS; ++iter) { // iterations
+    cerr << "ITERATION " << iter+1 << " (of max " << ITERATIONS << "), step size " << stepsize << " (of min " << minstepsize << "), weights: " << endl;
+    cerr << params << endl;
+
     paramhistory.push_back(params);
-    vector<string> context(5, "<s>"); // Hard-coded to be 6-gram perplexity
+    vector<string> context(context_size, "<s>"); // Hard-coded to be 6-gram perplexity
     double ppl = 0.0;
     DVector grad = DVector::Zero(nlambdas);
     DMatrix H = DMatrix::Zero(nlambdas, nlambdas);
@@ -107,12 +139,27 @@ void train_params(
         set_features(context, sentence[t], models, feats); // probs for actual n-gram
 
         double z = 0.0;
+        double maxlogprob=0.0; // Allows us to avoid overflow with negative params
         DVector expectfeats      = DVector::Zero(nlambdas);
         DMatrix expectfeatmatrix = DMatrix::Zero(nlambdas, nlambdas);
         DVector iterfeats = DVector::Zero(nlambdas); // Logically, this should be in the loop's scope
         for (unsigned i = 0; i < vocab.size(); ++i) { // probs over possible n-grams, for normalization
           set_features(context, vocab[i], models, iterfeats);
-          double us = exp(params.dot(iterfeats)); // measure
+          double logprob = params.dot(iterfeats);
+          if (i==0)
+            //maxlogprob=logprob;// more precise, less underflow
+            maxlogprob=0.0;// saves operations
+          else
+            if (logprob>maxlogprob)
+            {
+              double adjust = exp(maxlogprob-logprob);
+              z                *= adjust;
+              expectfeats      *= adjust;
+              expectfeatmatrix *= adjust;
+              maxlogprob=logprob;
+            }
+          double us = exp(params.dot(iterfeats)-maxlogprob); // measure
+          //double us = exp(params.dot(iterfeats)); // measure
 
           z                += us;
           expectfeats      += us * iterfeats;
@@ -127,9 +174,9 @@ void train_params(
         for (unsigned i = 0; i<context.size()-1; ++i)
           context[i]=context[i+1];
         context[context.size()-1]=sentence[t];
-        //context.push_back(sentence[t]);
+        //context.push_back(sentence[t]); // old code
 
-        // Perplexity
+        // Perplexity (actually log(perplexity))
         ppl  += params.dot(feats) - log(z);
         // Gradient
         grad += feats - expectfeats;
@@ -142,106 +189,210 @@ void train_params(
     // The gradient and Hessian coefficients cancel out, so don't really need to do this, but it's fast.
     grad *= -1.0/corpus.size();
     H    *= -1.0/corpus.size();
-    cerr << "ITERATION " << iter << "/" << ITERATIONS << ": log(PPL)=" << ppl << " PPL=" << exp(ppl) << endl;
-    cerr << "Input Weights: " << endl;
-    cerr << params << endl;
-    // Looks like we don't need the three lines below -- we can do it in-line
-    DMatrix Hnull = DMatrix::Zero(nlambdas-1, nlambdas-1);
-    Hnull=N.transpose()*H*N;
-    Eigen::SelfAdjointEigenSolver<DMatrix> eigensolver(Hnull);
-    cerr << "Eigenvalues:\n" << eigensolver.eigenvalues() << endl;
-    DVector deltaparams = -N*Hnull.colPivHouseholderQr().solve(N.transpose()*grad);
-    //DVector deltaparams = -N*(N.transpose()*H*N).colPivHouseholderQr().solve(N.transpose()*grad);
+    //cerr << "ITERATION " << iter << "/" << ITERATIONS << ": log(PPL)=" << ppl << " PPL=" << exp(ppl) << endl;
+    cerr << " log(PPL)=" << ppl << " PPL=" << exp(ppl) << endl;
+    //cerr << "Input Weights: " << endl;
+    //cerr << params << endl;
 
-    int reverttograd=0;
-    for (unsigned i = 0; i<nlambdas; i++)
-      if (params(i)+deltaparams(i)<0) // Can't do Newton step. Revert to descent.
-        reverttograd=1;
-    if (reverttograd==1)
+    if ((ppl<bestppl) || (iter==0))
     {
-      cerr << "Reverting to gradient, since Newton step infeasible." << endl;
-      deltaparams = -N*N.transpose()*grad; // projected gradient onto unit sum constraint
+      // Found a new best
+      bestppl=ppl;
+      bestparams=params;
+      double beststepsize=stepsize;
+      if (iter>0)
+        cerr << " New best point found, step size " << beststepsize << endl;
+
+      bestgrad=grad;
+      DVector deltaparams = DVector::Zero(nlambdas);
+
+      bool reverttograd=false;
+      bool solvesuccess=true;
+
+      // Find Newton step
+      if (AllowExtrapolation)
+      {
+        deltaparams = -H.colPivHouseholderQr().solve(grad);
+        Eigen::SelfAdjointEigenSolver<DMatrix> eigensolver(H);
+        cerr << "Eigenvalues (best if all positive):\n" << eigensolver.eigenvalues() << endl;
+        solvesuccess = grad.isApprox(-H*deltaparams);
+      }
+      else
+      {
+        bestgrad=N*N.transpose()*bestgrad; // Project gradient to interpolation space
+
+        // need to work in nullspace to maintain unit sum
+        DMatrix Hnull = DMatrix::Zero(nlambdas-1, nlambdas-1);
+
+        // Looks like we don't need the three lines below -- we can do it in-line (if we don't want eigenvalues)
+        Hnull=N.transpose()*H*N;
+        Eigen::SelfAdjointEigenSolver<DMatrix> eigensolver(Hnull);
+        cerr << "Eigenvalues (best if all positive):\n" << eigensolver.eigenvalues() << endl;
+        deltaparams = -N*Hnull.fullPivHouseholderQr().solve(N.transpose()*grad);
+        solvesuccess = (N.transpose()*grad).isApprox(-Hnull*deltaparams);
+      }
+      // eventually, params = bestparams + deltaparams;
+      if (solvesuccess)
+      {
+        stepsize=0.0;
+        for (unsigned i = 0; i<nlambdas; i++)
+          stepsize += deltaparams(i)*deltaparams(i);
+        stepsize=sqrt(stepsize); // holds length of Newton step
+        cerr << "Newton step, length " << stepsize << ": " << endl;
+        cerr << deltaparams << endl;
+
+        // Don't let the Newton step get much bigger than last successful step (likely would have to shrink)
+        if (stepsize>2.0*beststepsize)
+        {
+          stepsize=2.0*beststepsize;
+          reverttograd=true;
+          cerr << "Reverting to gradient, because Newton step is too large." << endl;
+        }
+      }
+      else
+      {
+        stepsize=2.0*beststepsize;
+        reverttograd=true;
+        cerr << "Reverting to gradient, because Newton step computation unsuccessful." << endl;
+      }
+      // Make the gradient unit norm, in feasible search direction.
+      if (!AllowNegativeParams)
+      {
+        // Project gradient to be a feasible search direction
+        vector<bool> active(nlambdas,false);
+        unsigned numactive=0;
+        for (unsigned i = 0; i<nlambdas; i++)
+          if ((bestparams(i)==0) && (bestgrad(i)>0)) // Project gradient to inactive constraints
+          {
+            active[i]=true;
+            bestgrad(i)=0.0;// Do this now, in case extrapolation allowed.
+            ++numactive;
+          }
+        if (numactive>0)
+        {
+          if (!AllowExtrapolation)
+          {
+            // Project gradient, for activity concerns
+            DMatrix tmpN = DMatrix::Constant(nlambdas,nlambdas-1, -1.0/sqrt((nlambdas-numactive-1)*(nlambdas-numactive-1)+nlambdas-numactive-1.0));
+            for (unsigned i=0; i<nlambdas-1; ++i)
+              tmpN(i,i)= tmpN(i,i)*(1.0-(nlambdas-numactive));
+            for (unsigned i=0; i<nlambdas; ++i)
+              if (active[i])
+                for (unsigned j=0; j<nlambdas-1; ++i)
+                  tmpN(i,j)=0;
+            bestgrad = -tmpN*tmpN.transpose()*bestgrad; // projected gradient onto unit sum and active set constraints
+          }
+        }
+      }
       double norm=0.0;
       for (unsigned i = 0; i<nlambdas; i++)
-        if ((params(i)==0) && (deltaparams(i)<0)) // Project gradient to inactive constraints
-        {
-          // Unfortunate if this happens, since it breaks the nullspace conditions
-          // Unit sum will be enforced crudely, later. TODO: improve, if it matters.
-          deltaparams(i)=0; 
-          cerr << "Projecting gradient to active constraint, weight " << i << endl;
-        }
-        else
-          norm += deltaparams(i)*deltaparams(i);
-      if (norm>0)
-        deltaparams /= sqrt(norm);
+        norm += bestgrad(i)*bestgrad(i);
+      if (norm!=0)
+        bestgrad /= sqrt(norm);
+      else
+      {
+        cerr << " Gradient is zero. Exiting.";
+        break;
+      }
+      cerr << "Gradient, unit length: " << endl;
+      cerr << bestgrad << endl;
+
+      // Find max step in gradient direction that remains feasible.
+      if (!AllowNegativeParams)
+      {
+        double limitfraction=0.5; // Not 1: If Newton step is bad, probably will need to reduce later anyway
+        for (unsigned i = 0; i<nlambdas; i++)
+          if (bestparams(i)-maxbestgradstep*bestgrad(i)<0)
+          {
+            double tmplimitfraction = bestparams(i)/(bestgrad(i)*maxbestgradstep);
+            if (tmplimitfraction<limitfraction)
+              limitfraction=tmplimitfraction;
+          }
+        maxbestgradstep=stepsize*limitfraction;
+        cerr << " Max grad step: " << maxbestgradstep << endl;
+      }
+      else
+        maxbestgradstep=stepsize;
+
+      if (!reverttograd)
+      {
+        if (!AllowNegativeParams)
+          for (unsigned i = 0; i<nlambdas; i++)
+            if (bestparams(i)+deltaparams(i)<0) // Can't do Newton step. Revert to descent.
+              reverttograd=true;
+        if (reverttograd)
+          cerr << "Reverting to gradient, since Newton step infeasible:" << endl;
+      }
+
+      if (reverttograd)
+      {
+        stepsize=maxbestgradstep;
+        deltaparams = -bestgrad * stepsize;
+      }
+
+      params=bestparams+deltaparams;
+      cerr << "Delta weights, step size " << stepsize << ": " << endl;
+      cerr << deltaparams << endl;
+      //cerr << "Safeguarded weights: " << endl;
+      //cerr << params << endl;
     }
-
-    cerr << "Delta weights: " << endl;
-    cerr << deltaparams << endl;
-    cerr << "Unsafeguarded weights (latest): " << endl;
-    cerr << params+deltaparams << endl;
-
-    // Collect history of function value, step, and smallest tried step size
-    deltaparamhistory.push_back(deltaparams);
-    pplhistory.push_back(ppl);
-    fractionhistory.push_back(-1); // Signifies no step yet taken.
-
-    unsigned minindex=pplhistory.size()-1;
-    for (unsigned i=0; i<pplhistory.size()-1; ++i)
-      if (pplhistory[i]<pplhistory[minindex])
-        minindex=i;
-    cerr << " Stepping from iteration " << minindex << endl;
-
-    if (fractionhistory[minindex]>0)
-      fractionhistory[minindex]*=0.5; // Step in this direction failed to decrease last time, so try again with smaller step
     else
     {
-      // First time to try a step. Make as large a feasible step as possible
-      fractionhistory[minindex]=1;
-      // We should be done (negative weights don't screw up the math), but they do screw
-      // up the numerics. We now explicitly disallow them:
-      for (unsigned i = 0; i<nlambdas; i++)
-        if (params(i)+deltaparams(i)<0)
-        {
-          double tmplimitfraction = params(i)/-deltaparams(i);
-          if (tmplimitfraction<fractionhistory[minindex])
-            fractionhistory[minindex]=tmplimitfraction;
-        }
+      // Last attempt failed at being better.
+      stepsize=std::min(stepdecreasefactor*stepsize,maxbestgradstep); // stepsize reduction factor is empirical
+      cerr << "Taking smaller step: " << stepsize << endl;
+      params = bestparams - bestgrad * stepsize;
     }
-
-    params = paramhistory[minindex] + fractionhistory[minindex]*deltaparamhistory[minindex];
-
+    // Clean them up.
     double sumparams=0.0;
     for (unsigned i = 0; i<nlambdas; i++)
     {
-      if (params(i)<1e-12)
-        params(i)=0;
+      if (!AllowNegativeParams)
+        if (params(i)<1e-12)
+          params(i)=0; // snap to zero, for active set and duplicate weights
       sumparams+= params(i);
     }
-    params /= sumparams;
+    if (!AllowExtrapolation)
+      params /= sumparams;
 
-    int duplicateentry=0;
-    for (unsigned i=0; i<pplhistory.size(); ++i)
+    unsigned dedupattempts=0;
+
+    bool duplicateentry=false;
+    for (unsigned i=0; i<paramhistory.size(); ++i)
       if (params==paramhistory[i])
-        duplicateentry=1;
-    if (duplicateentry==1)
+        duplicateentry=true;
+    while ((duplicateentry) && (dedupattempts<50))
     {
-      cerr << "Duplicate weight found! " << endl;
-      fractionhistory[minindex]*=0.5; // Step in this direction is duplicate, so try again with smaller step
-      params = paramhistory[minindex] + fractionhistory[minindex]*deltaparamhistory[minindex];
+      dedupattempts++;
+      cerr << "Duplicate weight found: " << endl;
+      cerr << params << endl;
+      stepsize*=0.5; // Step in this direction is duplicate, so try again with smaller step
+      params = bestparams - stepsize * bestgrad;
+
       sumparams=0.0;
       for (unsigned i = 0; i<nlambdas; i++)
       {
-        if (params(i)<1e-12)
-          params(i)=0;
+        if (!AllowNegativeParams)
+          if (params(i)<1e-12)
+            params(i)=0;
         sumparams+= params(i);
       }
-      params /= sumparams;
-    }
+      if (!AllowExtrapolation)
+        params /= sumparams;
 
-    cerr << "Safeguarded weights: " << endl;
-    cerr << params << endl;
-  } 
+      duplicateentry=false;
+      for (unsigned i=0; i<paramhistory.size(); ++i)
+        if (params==paramhistory[i])
+          duplicateentry=true;
+    }
+    if (stepsize<minstepsize)
+      break; // No need to make another step
+
+  }
+
+  cerr << "Training complete. Best weights:" << endl;
+  cerr << setprecision(16) << bestparams << endl;
+
 }
 
 int main(int argc, char** argv) {
