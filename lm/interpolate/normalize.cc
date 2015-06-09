734652cc9542b4fc8b3e0d8b99d415521f9a838b
Kenneth Heafield <github@kheafield.com>
2015-06-08 23:16:37 -0400

    Split off backoff matrix

--- lm/interpolate/normalize.cc
+++ lm/interpolate/normalize.cc
@@ -2,6 +2,7 @@
 
 #include "lm/common/compare.hh"
 #include "lm/common/ngram_stream.hh"
+#include "lm/interpolate/backoff_matrix.hh"
 #include "lm/interpolate/bounded_sequence_encoding.hh"
 #include "lm/interpolate/interpolate_info.hh"
 #include "lm/interpolate/merge_probabilities.hh"
@@ -18,26 +19,6 @@
 
 namespace lm { namespace interpolate {
 namespace {
-
-class BackoffMatrix {
-  public:
-    BackoffMatrix(std::size_t num_models, std::size_t max_order)
-      : max_order_(max_order), backing_(num_models * max_order) {
-      std::fill(backing_.begin(), backing_.end(), 0.0f);
-    }
-
-    float &Backoff(std::size_t model, std::size_t order_minus_1) {
-      return backing_[model * max_order_ + order_minus_1];
-    }
-
-    float Backoff(std::size_t model, std::size_t order_minus_1) const {
-      return backing_[model * max_order_ + order_minus_1];
-    }
-
-  private:
-    const std::size_t max_order_;
-    std::vector<float> backing_;
-};
 
 struct SuffixLexicographicLess : public std::binary_function<NGramHeader, NGramHeader, bool> {
   bool operator()(const NGramHeader first, const NGramHeader second) const {
