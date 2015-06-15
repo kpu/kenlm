e213b58316289c479d7ff0d514a913a0289a30fc
Kenneth Heafield <github@kheafield.com>
2015-06-14 23:02:09 -0400

    Should really be tune_file not test_file

--- lm/interpolate/tune_instance.hh
+++ lm/interpolate/tune_instance.hh
@@ -39,8 +39,8 @@ struct Instance {
   std::vector<WordIndex> extension_words;
 };
 
-// Takes ownership of test_file
-void LoadInstances(int test_file, const std::vector<StringPiece> &model_names, util::FixedArray<Instance> &instances, Matrix &ln_unigrams);
+// Takes ownership of tune_file
+void LoadInstances(int tune_file, const std::vector<StringPiece> &model_names, util::FixedArray<Instance> &instances, Matrix &ln_unigrams);
 
 }} // namespaces
 #endif // LM_INTERPOLATE_TUNE_INSTANCE_H
