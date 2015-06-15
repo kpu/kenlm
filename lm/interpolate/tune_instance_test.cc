9e906bb99d7f07167f1a182056824e59c2b7bf6f
Kenneth Heafield <github@kheafield.com>
2015-06-15 00:06:21 -0400

    More testing, including some failing tests

--- lm/interpolate/tune_instance_test.cc
+++ lm/interpolate/tune_instance_test.cc
@@ -36,6 +36,47 @@ BOOST_AUTO_TEST_CASE(Toy) {
   util::FixedArray<Instance> instances;
   Matrix ln_unigrams;
   LoadInstances(test_input.release(), model_names, instances, ln_unigrams);
+  // <unk>
+  BOOST_CHECK_CLOSE(-0.90309 * M_LN10, ln_unigrams(0, 0), 0.001);
+  BOOST_CHECK_CLOSE(-1 * M_LN10, ln_unigrams(0, 1), 0.001);
+  // <s>
+  BOOST_CHECK_EQUAL(-std::numeric_limits<double>::infinity(), ln_unigrams(1, 0));
+  BOOST_CHECK_EQUAL(-std::numeric_limits<double>::infinity(), ln_unigrams(1, 1));
+  // a
+  BOOST_CHECK_CLOSE(-0.46943438 * M_LN10, ln_unigrams(2, 0), 0.001);
+  BOOST_CHECK_CLOSE(-0.6146491 * M_LN10, ln_unigrams(2, 1), 0.001);
+  // </s>
+  BOOST_CHECK_CLOSE(-0.5720968 * M_LN10, ln_unigrams(3, 0), 0.001);
+  BOOST_CHECK_CLOSE(-0.6146491 * M_LN10, ln_unigrams(3, 1), 0.001);
+  // c
+  BOOST_CHECK_CLOSE(-0.90309 * M_LN10, ln_unigrams(4, 0), 0.001); // <unk>
+  BOOST_CHECK_CLOSE(-0.7659168 * M_LN10, ln_unigrams(4, 1), 0.001);
+  // too lazy to do b.
+
+  // Two instances:
+  // <s> predicts c
+  // <s> c predicts </s>
+  BOOST_REQUIRE_EQUAL(2, instances.size());
+  BOOST_CHECK_CLOSE(-0.30103 * M_LN10, instances[0].ln_backoff(0), 0.001);
+  BOOST_CHECK_CLOSE(-0.30103 * M_LN10, instances[0].ln_backoff(1), 0.001);
+
+  // Backoffs of <s> c
+  BOOST_CHECK_CLOSE(0.0, instances[1].ln_backoff(0), 0.001);
+  BOOST_CHECK_CLOSE((-0.30103 - 0.30103) * M_LN10, instances[1].ln_backoff(1), 0.001);
+
+  // Three extensions: a, b, c
+  BOOST_REQUIRE_EQUAL(3, instances[0].ln_extensions.rows());
+  BOOST_REQUIRE_EQUAL(3, instances[0].extension_words.size());
+
+  std::vector<WordIndex>::const_iterator row_a_it = std::find(instances[0].extension_words.begin(), instances[0].extension_words.end(), 2);
+  BOOST_REQUIRE(row_a_it != instances[0].extension_words.end());
+  WordIndex row_a = *row_a_it;
+  BOOST_CHECK_CLOSE(-0.37712017 * M_LN10, instances[0].ln_extensions(row_a, 0), 0.001);
+
+  // p_0(c | <s>) = p_0(c)b_0(<s>) = 10^(-0.90309 + -0.30103)
+  BOOST_CHECK_CLOSE((-0.90309 + -0.30103) * M_LN10, instances[0].ln_correct(0), 0.001);
+  // p_1(c | <s>) = 10^-0.4740302
+  BOOST_CHECK_CLOSE(-0.4740302 * M_LN10, instances[0].ln_correct(1), 0.001);
 }
 
 }}} // namespaces
