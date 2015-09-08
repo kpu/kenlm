bba5112e809843c64e4468684a05116c2de3e987
Kenneth Heafield <github@kheafield.com>
2015-09-08 15:33:29 +0100

    Get multiplier close to 0.5

--- util/probing_hash_table_benchmark_main.cc
+++ util/probing_hash_table_benchmark_main.cc
@@ -25,12 +25,14 @@ struct Entry {
   Key GetKey() const { return key; }
 };
 
-template <class Mod> bool Test(uint64_t entries, uint64_t lookups = 20000000, float multiplier = 1.5) {
+template <class Mod> bool Test(uint64_t entries, const std::vector<uint64_t> &look, float multiplier = 1.5) {
   typedef util::ProbingHashTable<Entry, util::IdentityHash, std::equal_to<Entry::Key>, Mod> Table;
   // Always round up to power of 2 for fair comparison.
   std::size_t size = Power2Mod::RoundBuckets(Table::Size(entries, multiplier) / sizeof(Entry)) * sizeof(Entry);
+  std::cerr << (static_cast<float>(size / sizeof(Entry)) / entries) << std::endl;
   scoped_malloc backing(util::CallocOrThrow(size));
   Table table(backing.get(), size);
+
   UTIL_TWISTER gen;
   UTIL_INT_DIST<uint64_t> dist(std::numeric_limits<uint64_t>::min(), std::numeric_limits<uint64_t>::max());
   double start = UserTime();
@@ -39,13 +41,30 @@ template <class Mod> bool Test(uint64_t entries, uint64_t lookups = 20000000, fl
     entry.key = dist(gen);
     table.Insert(entry);
   }
-  double inserted = UserTime();
+  double inserted = UserTime() - start;
   bool meaningless = true;
-  for (uint64_t i = 0; i < lookups; ++i) {
+  double before_lookup = UserTime();
+  for (uint64_t i = 0; i < look.size(); ++i) {
     typename Table::ConstIterator it;
-    meaningless ^= table.Find(dist(gen), it);
+    meaningless ^= table.Find(look[i], it);
   }
-  std::cout << entries << ' ' << size << ' ' << (inserted - start) / static_cast<double>(entries) << ' ' << (UserTime() - inserted) / static_cast<double>(lookups) << '\n';
+  std::cout << entries << ' ' << size << ' ' << inserted / static_cast<double>(entries) << ' ' << (UserTime() - before_lookup) / static_cast<double>(look.size()) << '\n';
+  return meaningless;
+}
+
+template <class Mod> bool TestRun(uint64_t lookups = 20000000, float multiplier = 1.5) {
+  std::vector<uint64_t> look;
+  look.reserve(lookups);
+  UTIL_TWISTER gen;
+  UTIL_INT_DIST<uint64_t> dist(std::numeric_limits<uint64_t>::min(), std::numeric_limits<uint64_t>::max());
+  for (uint64_t i = 0; i < lookups; ++i) {
+    look.push_back(dist(gen));
+  }
+
+  bool meaningless = true;
+  for (uint64_t i = 4; i <= 100000000ULL; i *= 4) {
+    meaningless ^= util::Test<Mod>(i / multiplier, look, multiplier);
+  }
   return meaningless;
 }
 
@@ -55,12 +74,8 @@ template <class Mod> bool Test(uint64_t entries, uint64_t lookups = 20000000, fl
 int main() {
   bool meaningless = false;
   std::cout << "#Integer division\n";
-  for (uint64_t i = 1; i <= 100000000ULL; i *= 10) {
-    meaningless ^= util::Test<util::DivMod>(i);
-  }
+  meaningless ^= util::TestRun<util::DivMod>();
   std::cout << "#Masking\n";
-  for (uint64_t i = 1; i <= 100000000ULL; i *= 10) {
-    meaningless ^= util::Test<util::Power2Mod>(i);
-  }
+  meaningless ^= util::TestRun<util::Power2Mod>();
   std::cerr << "Meaningless: " << meaningless << '\n';
 }
