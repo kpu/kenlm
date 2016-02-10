913c7ea10ed89cdaf873a2ef3095b61cb3d3c716
Kenneth Heafield <github@kheafield.com>
2016-02-10 16:37:08 +0000

    Remove debugging

--- lm/interpolate/tune_instance.cc
+++ lm/interpolate/tune_instance.cc
@@ -350,14 +350,12 @@ class ExtensionsFirstIteration {
           assert(!backoffs.empty());
           uint8_t max_order = backoffs.front().cols();
           for (util::stream::Stream stream(position); stream; ++stream) {
-            std::cerr << "ApplyBackoffs running" << std::endl;
             InitialExtension &ini = *reinterpret_cast<InitialExtension*>(stream.Get());
             assert(ini.order > 1); // If it's an extension, it should be higher than a unigram.
             if (ini.order != max_order) {
               ini.ext.ln_prob += backoffs[ini.ext.instance](ini.ext.model, ini.order - 1);
             }
           }
-          std::cerr << "ApplyBackoffs finished." << std::endl;
         }
 
       private:
