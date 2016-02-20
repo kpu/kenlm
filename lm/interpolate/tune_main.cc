0b87b1f36d07023c02021eff34fe7c4d08bb0bcf
Kenneth Heafield <github@kheafield.com>
2016-02-20 13:52:06 +0000

    First crack at main

--- lm/interpolate/tune_main.cc
+++ lm/interpolate/tune_main.cc
@@ -1,3 +1,4 @@
+#include "lm/common/size_option.hh"
 #include "lm/interpolate/tune_derivatives.hh"
 #include "lm/interpolate/tune_instances.hh"
 #include "util/file.hh"
@@ -12,11 +13,8 @@
 #include <iostream>
 
 namespace lm { namespace interpolate {
-void TuneWeights(int tune_file, const std::vector<StringPiece> &model_names, Vector &weights) {
-  util::FixedArray<Instance> instances;
-  Matrix ln_unigrams;
-  WordIndex bos = LoadInstances(tune_file, model_names, instances, ln_unigrams);
-  ComputeDerivative derive(instances, ln_unigrams, bos);
+void TuneWeights(int tune_file, const std::vector<StringPiece> &model_names, const InstancesConfig &config, Vector &weights) {
+  Instances instances(tune_file, model_names, config);
   weights = Vector::Constant(model_names.size(), 1.0 / model_names.size());
   Vector gradient;
   Matrix hessian;
@@ -27,7 +25,7 @@ void TuneWeights(int tune_file, const std::vector<StringPiece> &model_names, Vec
     }
     std::cerr << std::endl;
     std::cerr  << "Perplexity = " <<
-      derive.Iteration(weights, gradient, hessian)
+      Derivatives(instances, weights, gradient, hessian)
       << std::endl;
     // TODO: 1.0 step size was too big and it kept getting unstable.  More math.
     weights -= 0.7 * hessian.inverse() * gradient;
@@ -38,22 +36,38 @@ void TuneWeights(int tune_file, const std::vector<StringPiece> &model_names, Vec
 int main(int argc, char *argv[]) {
   Eigen::initParallel();
   namespace po = boost::program_options;
+  lm::interpolate::InstancesConfig config;
   // TODO help
   po::options_description options("Tuning options");
   std::string tuning_file;
   std::vector<std::string> input_models;
   options.add_options()
+    ("help,h", po::bool_switch(), "Show this help message")
     ("tuning,t", po::value<std::string>(&tuning_file)->required(), "File to tune on.  This should be a text file with one sentence per line.")
-    ("model,m", po::value<std::vector<std::string> >(&input_models)->multitoken()->required(), "Models to interpolate");
+    ("model,m", po::value<std::vector<std::string> >(&input_models)->multitoken()->required(), "Models to interpolate")
+    ("temp_prefix,T", po::value<std::string>(&config.sort.temp_prefix)->default_value("/tmp/lm"), "Temporary file prefix")
+    ("memory,S", lm::SizeOption(config.sort.total_memory, "1G"), "Memory to use in addition to unigram and backoff storage")
+    ("sort_block", lm::SizeOption(config.sort.buffer_size, "64M"), "Size of IO operations for sort (determines arity)");
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, options), vm);
+  if (argc == 1 || vm["help"].as<bool>()) {
+    std::cerr <<
+      "Tunes interpolation weights.\n\n"
+      << options << std::endl;
+    return 1;
+  }
+
   po::notify(vm);
+
+  config.lazy_memory = config.sort.total_memory;
+  config.extension_write_chain_mem = config.sort.total_memory;
+  config.model_read_chain_mem = config.sort.buffer_size;
 
   std::vector<StringPiece> model_names;
   for (std::vector<std::string>::const_iterator i = input_models.begin(); i != input_models.end(); ++i) {
     model_names.push_back(*i);
   }
   lm::interpolate::Vector weights;
-  lm::interpolate::TuneWeights(util::OpenReadOrThrow(tuning_file.c_str()), model_names, weights);
+  lm::interpolate::TuneWeights(util::OpenReadOrThrow(tuning_file.c_str()), model_names, config, weights);
   std::cout << weights.transpose() << std::endl;
 }
