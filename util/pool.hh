f1a82e985e60dc3a9cd01b344b4d2b45b899b0d8
Kenneth Heafield <github@kheafield.com>
2016-02-21 17:02:19 +0000

    Add a pool with a linked list based free list

--- util/pool.hh
+++ util/pool.hh
@@ -1,16 +1,16 @@
-// Very simple pool.  It can only allocate memory.  And all of the memory it
-// allocates must be freed at the same time.  
-
 #ifndef UTIL_POOL_H
 #define UTIL_POOL_H
 
+#include <cstring>
 #include <vector>
 
 #include <stdint.h>
-#include <string.h>
 
 namespace util {
 
+/* Very simple pool.  It can only allocate memory.  And all of the memory it
+ * allocates must be freed at the same time.
+ */
 class Pool {
   public:
     Pool();
@@ -34,7 +34,7 @@ class Pool {
       if (current_ > current_end_) {
         std::size_t new_total = current_ - static_cast<uint8_t*>(base);
         void *new_base = More(new_total);
-        memcpy(new_base, base, new_total - additional);
+        std::memcpy(new_base, base, new_total - additional);
         base = new_base;
       } 
     }
@@ -51,7 +51,42 @@ class Pool {
     // no copying
     Pool(const Pool &);
     Pool &operator=(const Pool &);
-}; 
+};
+
+/**
+ * Pool designed to allow limited freeing.
+ * Keeps a linked list of free elements in the free spaces.
+ * Will not reduce in size until FreeAll is called.
+ */
+class FreePool {
+  public:
+    explicit FreePool(std::size_t element_size)
+      : free_list_(NULL), element_size_(element_size) {}
+
+    void *Allocate() {
+      if (free_list_) {
+        void *ret = free_list_;
+        free_list_ = *reinterpret_cast<void**>(free_list_);
+        return ret;
+      } else {
+        return backing_.Allocate(element_size_);
+      }
+    }
+
+    void Free(void *ptr) {
+      *reinterpret_cast<void**>(ptr) = free_list_;
+      free_list_ = ptr;
+    }
+
+    std::size_t ElementSize() const { return element_size_; }
+
+  private:
+    void *free_list_;
+
+    Pool backing_;
+
+    const std::size_t element_size_;
+};
 
 } // namespace util
 
