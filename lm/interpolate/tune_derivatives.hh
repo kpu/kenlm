727ef9d6da5f3cb1a4bb03dfc85fd1058feaa785
Kenneth Heafield <github@kheafield.com>
2016-02-28 22:24:45 +0000

    Attempt at SGD.
    Seems to be faster, need to test convergence more.
    Does not randomize order of examples.
    Apportions summed correct term over examples instead of using the one due to the example (this might be a bug or a feature)

--- lm/interpolate/tune_derivatives.hh
+++ lm/interpolate/tune_derivatives.hh
@@ -12,7 +12,7 @@ class Instances;
 
 // Given tuning instances and model weights, computes the objective function (log probability), gradient, and Hessian.
 // Returns log probability / number of instances.
-Accum Derivatives(Instances &instances /* Doesn't modify but ReadExtensions is lazy */, const Vector &weights, Vector &gradient, Matrix &hessian);
+void Derivatives(Instances &instances /* Doesn't modify but ReadExtensions is lazy */, Vector &weights);
 
 }} // namespaces
 
--- lm/interpolate/tune_weights.cc
+++ lm/interpolate/tune_weights.cc
@@ -16,17 +16,9 @@ namespace lm { namespace interpolate {
 void TuneWeights(int tune_file, const std::vector<StringPiece> &model_names, const InstancesConfig &config, std::vector<float> &weights_out) {
   Instances instances(tune_file, model_names, config);
   Vector weights = Vector::Constant(model_names.size(), 1.0 / model_names.size());
-  Vector gradient;
-  Matrix hessian;
   for (std::size_t iteration = 0; iteration < 10 /*TODO fancy stopping criteria */; ++iteration) {
-    std::cerr << "Iteration " << iteration << ": weights =";
-    for (Vector::Index i = 0; i < weights.rows(); ++i) {
-      std::cerr << ' ' << weights(i);
-    }
-    std::cerr << std::endl;
-    std::cerr << "Perplexity = " << Derivatives(instances, weights, gradient, hessian) << std::endl;
-    // TODO: 1.0 step size was too big and it kept getting unstable.  More math.
-    weights -= 0.7 * hessian.inverse() * gradient;
+    std::cerr << "Iteration " << iteration << ": weights = " << weights.transpose() << std::endl;
+    Derivatives(instances, weights);
   }
   weights_out.assign(weights.data(), weights.data() + weights.size());
 }
