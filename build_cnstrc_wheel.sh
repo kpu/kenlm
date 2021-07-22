dea8f2b1ae19663db903c2b61be26f12fee56e3e
Narek Gharibyan <narekgharibyan@gmail.com>
2021-07-22 10:29:39 +0400

    - output to wheels dir

--- build_cnstrc_wheel.sh
+++ build_cnstrc_wheel.sh
@@ -11,5 +11,5 @@ cd build && cmake .. && make -j 8 && cd ..
 # copy binaries into kenlm_bin dir
 cp -r build/bin kenlm_bin/
 
-# build the cnstrc_kenlm wheel
-pip wheel .
+# build the cnstrc_kenlm wheels
+pip wheel . -w wheels
