a765d2047cb71f02846e4bda9898a7e06a8ae2bb
Narek Gharibyan <narekgharibyan@gmail.com>
2021-07-22 10:05:59 +0400

    - added call()

--- kenlm_bin/__init__.py
+++ kenlm_bin/__init__.py
@@ -1,5 +1,9 @@
 import pkg_resources
+import subprocess
+
+from typing import List
 
 
-def p_path():
-    return pkg_resources.resource_filename
+def call(executable: str, args_list: List[str], **popen_kwargs) -> int:
+    executable_path = pkg_resources.resource_filename('kenlm_bin', f'bin/{executable}')
+    return subprocess.call([executable_path] + args_list, **popen_kwargs)
