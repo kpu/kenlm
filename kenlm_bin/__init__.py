22ba05ddc010fe87f21a3dbdff77fd47d6c980dc
Narek Gharibyan <narekgharibyan@gmail.com>
2021-07-22 10:23:51 +0400

    - added error message

--- kenlm_bin/__init__.py
+++ kenlm_bin/__init__.py
@@ -1,3 +1,4 @@
+import os
 import pkg_resources
 import subprocess
 
@@ -6,4 +7,7 @@ from typing import List
 
 def call(executable: str, args_list: List[str], **popen_kwargs) -> int:
     executable_path = pkg_resources.resource_filename('kenlm_bin', f'bin/{executable}')
+    if not os.path.exists(executable_path):
+        raise Exception(f'Executable "{executable}" does not exist.')
+
     return subprocess.call([executable_path] + args_list, **popen_kwargs)
