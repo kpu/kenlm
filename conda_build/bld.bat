f0c74ea91b72a919615e2466b5351e214675b301
Cristian García Romero <cgr71ii@gmail.com>
2022-02-23 17:21:11 +0100

    Fix and update conda build scripts

--- conda_build/bld.bat
+++ conda_build/bld.bat
@@ -6,15 +6,16 @@ set DIR="%RECIPE_DIR%/%RDIR%"
 echo "DIR: %DIR%"
 echo "KENLM_MAX_ORDER: %KENLM_MAX_ORDER%"
 
-mkdir %DIR%/build_conda
+if exist %DIR%/build ( rmdir /S /Q %DIR%/build )
+mkdir %DIR%/build
 
 echo " - Building..."
-cd %DIR%/build_conda
+cd %DIR%/build
 cmake .. -DKENLM_MAX_ORDER=%KENLM_MAX_ORDER% -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%
 echo " - Installing..."
 make -j all install
 cd %DIR%
-rmdir /S /Q %DIR%/build_conda
+rmdir /S /Q %DIR%/build
 
 echo " - Installing python module..."
 "%PYTHON%" -m pip install "%DIR%" --install-option="--max_order %KENLM_MAX_ORDER%"
--- conda_build/build.sh
+++ conda_build/build.sh
@@ -4,15 +4,16 @@ DIR="${RECIPE_DIR}/${RDIR}"
 echo "DIR: $DIR"
 echo "KENLM_MAX_ORDER: $KENLM_MAX_ORDER"
 
-mkdir -p $DIR/build_conda
+[[ -d $DIR/build ]] && rm -rf $DIR/build
+mkdir -p $DIR/build
 
 echo -e " - \e[4mBuilding...\e[0m"
-cd $DIR/build_conda
+cd $DIR/build
 cmake .. -DKENLM_MAX_ORDER=$KENLM_MAX_ORDER -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
 echo -e " - \e[4mInstalling...\e[0m"
 make -j all install
 cd $DIR
-rm -rf $DIR/build_conda
+rm -rf $DIR/build
 
 echo -e " - \e[4mInstalling python module...\e[0m"
 $PYTHON -m pip install $DIR --install-option="--max_order $KENLM_MAX_ORDER"
