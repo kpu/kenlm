b85f582632c2260d45d2d5d662233859f19ec9b4
Jiang Nan <91359667+NeuroSymbol@users.noreply.github.com>
2024-07-23 13:45:11 +0800

    Update model.hh

--- lm/model.hh
+++ lm/model.hh
@@ -35,6 +35,9 @@ template <class Search, class VocabularyT> class GenericModel : public base::Mod
     static const ModelType kModelType;
 
     static const unsigned int kVersion = Search::kVersion;
+    
+    // 新增的 predict_next 方法声明
+    std::pair<std::string, float> predict_next(const std::string &context) const;
 
     /* Get the size of memory that will be mapped given ngram counts.  This
      * does not include small non-mapped control structures, such as this class
